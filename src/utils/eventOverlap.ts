5ddd517af514a824ec71619cd41f75b9c2d46850
hanbeulYou <hanbeul.you@gmail.com>
2025-05-12 00:42:00 +0900

    docs: parseDateTime 함수 JSDoc 설명 추가

--- src/utils/eventOverlap.ts
+++ src/utils/eventOverlap.ts
@@ -1,5 +1,9 @@
 import { Event, EventForm } from '../types';
 
+/**
+ * 주어진 날짜와 시간을 Date 객체로 변환합니다.
+ * 날짜 문자열이 비어있거나 시간 문자열이 비어있으면 Invalid Date를 반환합니다.
+ */
 export function parseDateTime(date: string, time: string) {
   return new Date(`${date}T${time}`);
 }
