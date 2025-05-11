cac6966ac8733b6ce3670fc7c8b4523f660bb049
DohyungKim <ehgud456456@naver.com>
2025-05-11 22:47:10 +0900

    feat: test for `convertEventToDateRange`

--- src/__tests__/unit/easy.eventOverlap.spec.ts
+++ src/__tests__/unit/easy.eventOverlap.spec.ts
@@ -1,3 +1,4 @@
+import realEvents from '../../__mocks__/response/realEvents.json';
 import { Event } from '../../types';
 import {
   convertEventToDateRange,
@@ -34,11 +35,74 @@ describe('parseDateTime', () => {
 });
 
 describe('convertEventToDateRange', () => {
-  it('일반적인 이벤트를 올바른 시작 및 종료 시간을 가진 객체로 변환한다', () => {});
+  it('일반적인 이벤트를 올바른 시작 및 종료 시간을 가진 객체로 변환한다', () => {
+    const event: Event = {
+      id: '09702fb3-a478-40b3-905e-9ab3c8849dcd',
+      title: '점심 약속',
+      date: '2025-05-21',
+      startTime: '12:30',
+      endTime: '13:30',
+      description: '동료와 점심 식사',
+      location: '회사 근처 식당',
+      category: '개인',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    };
 
-  it('잘못된 날짜 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {});
+    const result = convertEventToDateRange(event);
 
-  it('잘못된 시간 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {});
+    expect(result.start).toBeInstanceOf(Date);
+    expect(result.start.getFullYear()).toBe(2025);
+    expect(result.start.getMonth()).toBe(4);
+    expect(result.start.getDate()).toBe(21);
+    expect(result.start.getHours()).toBe(12);
+    expect(result.start.getMinutes()).toBe(30);
+
+    expect(result.end).toBeInstanceOf(Date);
+    expect(result.end.getFullYear()).toBe(2025);
+    expect(result.end.getMonth()).toBe(4);
+    expect(result.end.getDate()).toBe(21);
+    expect(result.end.getHours()).toBe(13);
+    expect(result.end.getMinutes()).toBe(30);
+  });
+
+  it('잘못된 날짜 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {
+    const event: Event = {
+      id: '09702fb3-a478-40b3-905e-9ab3c8849dcd',
+      title: '점심 약속',
+      date: '2025-055-21',
+      startTime: '12:30',
+      endTime: '13:30',
+      description: '동료와 점심 식사',
+      location: '회사 근처 식당',
+      category: '개인',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    };
+
+    const result = convertEventToDateRange(event);
+    expect(result.start.toString()).toBe('Invalid Date');
+    expect(result.end.toString()).toBe('Invalid Date');
+  });
+
+  it('잘못된 시간 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {
+    const event: Event = {
+      id: '09702fb3-a478-40b3-905e-9ab3c8849dcd',
+      title: '점심 약속',
+      date: '2025-05-21',
+      startTime: '123:30:60',
+      endTime: '123:30:60',
+      description: '동료와 점심 식사',
+      location: '회사 근처 식당',
+      category: '개인',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    };
+
+    const result = convertEventToDateRange(event);
+    expect(result.start.toString()).toBe('Invalid Date');
+    expect(result.end.toString()).toBe('Invalid Date');
+  });
 });
 
 describe('isOverlapping', () => {
