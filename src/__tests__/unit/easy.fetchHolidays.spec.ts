615d3dfb9d3c6fda3a693301f873a12cddac1604
chaaerim <cofla411@ewhain.net>
2025-05-11 23:24:16 +0900

    feat: fetchHolidays util test code 추가

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -1,9 +1,27 @@
 import { fetchHolidays } from '../../apis/fetchHolidays';
 
 describe('fetchHolidays', () => {
-  it('주어진 월의 공휴일만 반환한다', () => {});
+  it('주어진 월의 공휴일만 반환한다', () => {
+    const date = new Date(2025, 7, 1);
+    const result = fetchHolidays(date);
+    expect(result).toEqual({ '2025-08-15': '광복절' });
+  });
 
-  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {});
+  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {
+    const date = new Date(2025, 6, 1);
+    const result = fetchHolidays(date);
+    expect(result).toEqual({});
+  });
 
-  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {});
+  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    const date = new Date(2025, 9, 1);
+    const result = fetchHolidays(date);
+    expect(result).toEqual({
+      '2025-10-03': '개천절',
+      '2025-10-05': '추석',
+      '2025-10-06': '추석',
+      '2025-10-07': '추석',
+      '2025-10-09': '한글날',
+    });
+  });
 });
