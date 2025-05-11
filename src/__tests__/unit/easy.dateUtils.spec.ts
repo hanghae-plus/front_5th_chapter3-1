db2fefd9e1f8d7c616a496f37791776122d72b69
hanbeulYou <hanbeul.you@gmail.com>
2025-05-11 21:19:28 +0900

    test: dateUtils 유닛테스트 추가

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -366,11 +366,27 @@ describe('fillZero', () => {
 });
 
 describe('formatDate', () => {
-  it('날짜를 YYYY-MM-DD 형식으로 포맷팅한다', () => {});
+  it('날짜를 YYYY-MM-DD 형식으로 포맷팅한다', () => {
+    const date = new Date('2025-07-10');
+    const formattedDate = formatDate(date);
+    expect(formattedDate).toBe('2025-07-10');
+  });
 
-  it('day 파라미터가 제공되면 해당 일자로 포맷팅한다', () => {});
+  it('day 파라미터가 제공되면 해당 일자로 포맷팅한다', () => {
+    const date = new Date('2025-07-10');
+    const formattedDate = formatDate(date, 1);
+    expect(formattedDate).toBe('2025-07-01');
+  });
 
-  it('월이 한 자리 수일 때 앞에 0을 붙여 포맷팅한다', () => {});
+  it('월이 한 자리 수일 때 앞에 0을 붙여 포맷팅한다', () => {
+    const date = new Date('2025-7-10');
+    const formattedDate = formatDate(date, 1);
+    expect(formattedDate).toBe('2025-07-01');
+  });
 
-  it('일이 한 자리 수일 때 앞에 0을 붙여 포맷팅한다', () => {});
+  it('일이 한 자리 수일 때 앞에 0을 붙여 포맷팅한다', () => {
+    const date = new Date('2025-10-1');
+    const formattedDate = formatDate(date, 1);
+    expect(formattedDate).toBe('2025-10-01');
+  });
 });
