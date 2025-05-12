450bdb58fd2d4602152a08297b75c7d3674196b4
Jin Kwan Kim <kkole3897@gmail.com>
2025-05-13 00:20:34 +0900

    test: getWeeksAtMonth 테스트 추가

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -132,7 +132,24 @@ describe('getWeekDates', () => {
 });
 
 describe('getWeeksAtMonth', () => {
-  it('2025년 7월 1일의 올바른 주 정보를 반환해야 한다', () => {});
+  it('2025년 7월 1일의 올바른 주 정보를 반환해야 한다', () => {
+    const date = new Date(2025, 6, 1);
+    const expected = [
+      [null, null, 1, 2, 3, 4, 5],
+      [6, 7, 8, 9, 10, 11, 12],
+      [13, 14, 15, 16, 17, 18, 19],
+      [20, 21, 22, 23, 24, 25, 26],
+      [27, 28, 29, 30, 31, null, null],
+    ];
+
+    const weeks = getWeeksAtMonth(date);
+
+    weeks.forEach((week, i) => {
+      week.forEach((day, j) => {
+        expect(day).toBe(expected[i][j]);
+      });
+    });
+  });
 });
 
 describe('getEventsForDay', () => {
