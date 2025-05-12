665d61ab1e2b2fa50ea35b5b59e27d6c1205e8ce
hanbeulYou <hanbeul.you@gmail.com>
2025-05-12 14:07:23 +0900

    test: useCalendarView view 유닛테스트 추가

--- src/__tests__/hooks/easy.useCalendarView.spec.ts
+++ src/__tests__/hooks/easy.useCalendarView.spec.ts
@@ -44,16 +44,50 @@ describe('useCalendarView 내부 동작', () => {
   it("주간 뷰에서 다음으로 navigate시 7일 후 '2025-10-08' 날짜로 지정이 된다", () => {
     const { result } = renderHook(() => useCalendarView());
     act(() => {
+      result.current.setView('week');
+    });
+    act(() => {
       result.current.navigate('next');
     });
-    expect(result.current.currentDate).toBe(new Date('2025-10-08'));
+    expect(result.current.currentDate).toEqual(new Date('2025-10-08'));
   });
 
-  it("주간 뷰에서 이전으로 navigate시 7일 후 '2025-09-24' 날짜로 지정이 된다", () => {});
+  it("주간 뷰에서 이전으로 navigate시 7일 후 '2025-09-24' 날짜로 지정이 된다", () => {
+    const { result } = renderHook(() => useCalendarView());
+    act(() => {
+      result.current.setView('week');
+    });
+    act(() => {
+      result.current.navigate('prev');
+    });
+    expect(result.current.currentDate).toEqual(new Date('2025-09-24'));
+  });
 
-  it("월간 뷰에서 다음으로 navigate시 한 달 전 '2025-11-01' 날짜여야 한다", () => {});
+  it("월간 뷰에서 다음으로 navigate시 한 달 전 '2025-11-01' 날짜여야 한다", () => {
+    const { result } = renderHook(() => useCalendarView());
+    act(() => {
+      result.current.navigate('next');
+    });
+    expect(result.current.currentDate).toEqual(new Date('2025-11-01'));
+  });
 
-  it("월간 뷰에서 이전으로 navigate시 한 달 전 '2025-09-01' 날짜여야 한다", () => {});
+  it("월간 뷰에서 이전으로 navigate시 한 달 전 '2025-09-01' 날짜여야 한다", () => {
+    const { result } = renderHook(() => useCalendarView());
+    act(() => {
+      result.current.navigate('prev');
+    });
+    expect(result.current.currentDate).toEqual(new Date('2025-09-01'));
+  });
 
-  it("currentDate가 '2025-01-01' 변경되면 1월 휴일 '신정'으로 업데이트되어야 한다", async () => {});
+  // 25년 1월의 경우 '신정' 뿐만 아니라 '설날'이 포함되어 있어, 테스트 설명이 수정되어야 합니다.
+  // '신정', '설날' 모두 포함시키기에는 테스트 설명이 복잡해질 것 같아, 5월로 바꿨습니다.
+  it("currentDate가 '2025-05-05' 변경되면 5월 휴일 '어린이날'로 업데이트되어야 한다", async () => {
+    const { result } = renderHook(() => useCalendarView());
+    act(() => {
+      result.current.setCurrentDate(new Date('2025-05-05'));
+    });
+    expect(result.current.holidays).toEqual({
+      '2025-05-05': '어린이날',
+    });
+  });
 });
