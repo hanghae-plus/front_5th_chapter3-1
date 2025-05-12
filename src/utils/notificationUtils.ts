1a830cffd73e9019a75e6c3feed8c82d0f609bc1
unseoJang <qqbck123@naver.com>
2025-05-13 02:24:34 +0900

    test: getUpcomingEvents 테스트 코드 제작 완료

--- src/utils/notificationUtils.ts
+++ src/utils/notificationUtils.ts
@@ -3,6 +3,13 @@ import { Event } from '../types';
 const 초 = 1000;
 const 분 = 초 * 60;
 
+/**
+ * 알림 시간이 정확히 도래한 이벤트를 반환
+ * @param events
+ * @param now
+ * @param notifiedEvents
+ * @returns
+ */
 export function getUpcomingEvents(events: Event[], now: Date, notifiedEvents: string[]) {
   return events.filter((event) => {
     const eventStart = new Date(`${event.date}T${event.startTime}`);
