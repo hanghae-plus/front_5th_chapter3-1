d2b187a730d84886866e3629ea0ecb34a34b7695
JiYu <eunjun1018@gmail.com>
2025-05-12 11:41:18 +0900

    timeValidation 테스트코드 작성

--- src/__tests__/unit/easy.timeValidation.spec.ts
+++ src/__tests__/unit/easy.timeValidation.spec.ts
@@ -1,15 +1,46 @@
-import { getTimeErrorMessage } from '../../utils/timeValidation';
+import { getTimeErrorMessage } from '../../utils/timeValidation'
 
 describe('getTimeErrorMessage >', () => {
-  it('시작 시간이 종료 시간보다 늦을 때 에러 메시지를 반환한다', () => {});
+  it('시작 시간이 종료 시간보다 늦을 때 에러 메시지를 반환한다', () => {
+    const result = getTimeErrorMessage('13:00', '12:00')
+    expect(result.startTimeError).toBe('시작 시간은 종료 시간보다 빨라야 합니다.')
+    expect(result.endTimeError).toBe('종료 시간은 시작 시간보다 늦어야 합니다.')
+  })
 
-  it('시작 시간과 종료 시간이 같을 때 에러 메시지를 반환한다', () => {});
+  it('시작 시간과 종료 시간이 같을 때 에러 메시지를 반환한다', () => {
+    const result = getTimeErrorMessage('12:00', '12:00')
+    expect(result.startTimeError).toBe('시작 시간은 종료 시간보다 빨라야 합니다.')
+    expect(result.endTimeError).toBe('종료 시간은 시작 시간보다 늦어야 합니다.')
+  })
 
-  it('시작 시간이 종료 시간보다 빠를 때 null을 반환한다', () => {});
+  it('시작 시간이 종료 시간보다 빠를 때 null을 반환한다', () => {
+    const result = getTimeErrorMessage('11:00', '12:00')
+    expect(result.startTimeError).toBeNull()
+    expect(result.endTimeError).toBeNull()
+  })
 
-  it('시작 시간이 비어있을 때 null을 반환한다', () => {});
+  it('시작 시간이 비어있을 때 null을 반환한다', () => {
+    const result = getTimeErrorMessage('', '12:00')
+    expect(result.startTimeError).toBeNull()
+    expect(result.endTimeError).toBeNull()
+  })
 
-  it('종료 시간이 비어있을 때 null을 반환한다', () => {});
+  it('종료 시간이 비어있을 때 null을 반환한다', () => {
+    const result = getTimeErrorMessage('12:00', '')
+    expect(result.startTimeError).toBeNull()
+    expect(result.endTimeError).toBeNull()
+  })
 
-  it('시작 시간과 종료 시간이 모두 비어있을 때 null을 반환한다', () => {});
-});
+  it('시작 시간과 종료 시간이 모두 비어있을 때 null을 반환한다', () => {
+    const result = getTimeErrorMessage('', '')
+    expect(result.startTimeError).toBeNull()
+    expect(result.endTimeError).toBeNull()
+  })
+})
+
+// toBeNull()은 값이 null인지 확인
+
+// toBe()는 객체의 (===)을 확인
+// 원시값(string, number, boolean 등)이 정확히 같은지 확인할 때 사용
+
+// 객체나 배열 비교에는 toBe()가 아닌 toEqual() 또는 toStrictEqual()을 사용
