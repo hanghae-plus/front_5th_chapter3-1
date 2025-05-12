5824af93f272878917cfa91e1433924c2de80299
hanbeulYou <hanbeul.you@gmail.com>
2025-05-13 01:49:46 +0900

    chore: handlers 함수 console.log 및 주석 삭제

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -10,9 +10,6 @@ import {
 } from './handlersUtils';
 import { events } from './response/events.json' assert { type: 'json' };
 
-// ! HARD
-// ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
-
 setupMockHandlerCreation(events as Event[]);
 
 export const handlers = [
@@ -23,9 +20,6 @@ export const handlers = [
   http.post('/api/events', async ({ request }) => {
     const event = (await request.json()) as Event;
     const newEvent = { ...event, id: String(events.length + 1) };
-    console.log('====================================');
-    console.log(newEvent);
-    console.log('====================================');
     setupMockHandlerAppend(newEvent);
     return HttpResponse.json(newEvent);
   }),
@@ -38,11 +32,8 @@ export const handlers = [
       (event): Event =>
         Number(event.id) === Number(id) ? ({ ...event, ...update } as Event) : (event as Event)
     );
-    console.log('====================================');
-    console.log(updatedEvent);
-    console.log('====================================');
     setupMockHandlerUpdateById(updatedEvent);
-    return HttpResponse.json(event);
+    return HttpResponse.json(updatedEvent);
   }),
 
   http.delete('/api/events/:id', ({ params }) => {
