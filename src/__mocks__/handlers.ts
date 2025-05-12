bb094a6071ac3f29e4e759ba457188a5478ff78c
DohyungKim <ehgud456456@naver.com>
2025-05-13 01:00:03 +0900

    feat: msw 핸들러 구현

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -3,16 +3,31 @@ import { http, HttpResponse } from 'msw';
 import { Event } from '../types';
 import { events } from './response/events.json' assert { type: 'json' };
 
+let mockEvents = [...events];
+
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
   http.get('/api/events', () => {
-    return HttpResponse.json({ events });
+    return HttpResponse.json({ events: mockEvents }, { status: 200 });
   }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  http.post('/api/events', async ({ request }) => {
+    const newEvent = (await request.json()) as Event;
+    mockEvents.push(newEvent);
+    return HttpResponse.json({ success: true }, { status: 201 });
+  }),
 
-  http.put('/api/events/:id', async () => {}),
+  http.put('/api/events/:id', async ({ request, params }) => {
+    const { id } = params;
+    const updatedEvent = (await request.json()) as Event;
+    mockEvents = mockEvents.map((event) => (event.id === id ? updatedEvent : event));
+    return HttpResponse.json({ success: true }, { status: 200 });
+  }),
 
-  http.delete('/api/events/:id', ({ params }) => {}),
+  http.delete('/api/events/:id', ({ params }) => {
+    const { id } = params;
+    mockEvents = mockEvents.filter((event) => event.id !== id);
+    return HttpResponse.json({ success: true }, { status: 200 });
+  }),
 ];
