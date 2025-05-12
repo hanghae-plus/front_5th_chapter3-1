4066865f1ead77db4d6944f0af734082bb8befc8
Jin Kwan Kim <kkole3897@gmail.com>
2025-05-13 01:08:57 +0900

    test: convertEventToDateRange 테스트 추가

--- src/__tests__/unit/easy.eventOverlap.spec.ts
+++ src/__tests__/unit/easy.eventOverlap.spec.ts
@@ -53,11 +53,81 @@ describe('parseDateTime', () => {
 });
 
 describe('convertEventToDateRange', () => {
-  it('일반적인 이벤트를 올바른 시작 및 종료 시간을 가진 객체로 변환한다', () => {});
+  it('일반적인 이벤트를 올바른 시작 및 종료 시간을 가진 객체로 변환한다', () => {
+    const event: Event = {
+      id: '1',
+      title: '테스트 이벤트',
+      date: '2025-07-01',
+      startTime: '14:30',
+      endTime: '15:30',
+      description: '테스트 이벤트 설명',
+      location: '테스트 장소',
+      category: '테스트 카테고리',
+      repeat: {
+        type: 'none',
+        interval: 0,
+      },
+      notificationTime: 0,
+    };
 
-  it('잘못된 날짜 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {});
+    const result = convertEventToDateRange(event);
 
-  it('잘못된 시간 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {});
+    // QUESTION: 유닛 테스트에서 다른 함수를 불러서 테스트하는게 옳은 방법인지?
+    expect(result).toEqual({
+      start: parseDateTime(event.date, event.startTime),
+      end: parseDateTime(event.date, event.endTime),
+    });
+  });
+
+  it('잘못된 날짜 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {
+    const event: Event = {
+      id: '1',
+      title: '테스트 이벤트',
+      date: '2025-07-200',
+      startTime: '14:30',
+      endTime: '15:30',
+      description: '테스트 이벤트 설명',
+      location: '테스트 장소',
+      category: '테스트 카테고리',
+      repeat: {
+        type: 'none',
+        interval: 0,
+      },
+      notificationTime: 0,
+    };
+
+    const result = convertEventToDateRange(event);
+
+    expect(result.start).toBeInstanceOf(Date);
+    expect(result.start.getTime()).toBeNaN();
+    expect(result.end).toBeInstanceOf(Date);
+    expect(result.end.getTime()).toBeNaN();
+  });
+
+  it('잘못된 시간 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {
+    const event: Event = {
+      id: '1',
+      title: '테스트 이벤트',
+      date: '2025-07-01',
+      startTime: '25:30',
+      endTime: '30:30',
+      description: '테스트 이벤트 설명',
+      location: '테스트 장소',
+      category: '테스트 카테고리',
+      repeat: {
+        type: 'none',
+        interval: 0,
+      },
+      notificationTime: 0,
+    };
+
+    const result = convertEventToDateRange(event);
+
+    expect(result.start).toBeInstanceOf(Date);
+    expect(result.start.getTime()).toBeNaN();
+    expect(result.end).toBeInstanceOf(Date);
+    expect(result.end.getTime()).toBeNaN();
+  });
 });
 
 describe('isOverlapping', () => {
