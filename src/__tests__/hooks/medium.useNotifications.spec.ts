51bfb7f26df2eef66a277f818718a9157fc9d48f
hanbeulYou <hanbeul.you@gmail.com>
2025-05-14 02:09:13 +0900

    test: 알림 중복 방지 로직 테스트 추가

--- src/__tests__/hooks/medium.useNotifications.spec.ts
+++ src/__tests__/hooks/medium.useNotifications.spec.ts
@@ -56,4 +56,32 @@ it('index를 기준으로 알림을 적절하게 제거할 수 있다', async ()
   expect(result.current.notifications).toEqual([]);
 });
 
-it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', () => {});
+it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', async () => {
+  vi.useFakeTimers();
+  vi.setSystemTime('2025-07-01T08:50:00');
+
+  const events = [...eventsData.events] as Event[];
+  const { result } = renderHook(() => useNotifications(events));
+
+  await act(async () => {
+    vi.advanceTimersByTime(1000);
+  });
+
+  expect(result.current.notifications).toEqual([
+    {
+      id: events[0].id,
+      message: createNotificationMessage(events[0]),
+    },
+  ]);
+
+  await act(async () => {
+    vi.advanceTimersByTime(1000);
+  });
+
+  expect(result.current.notifications).toEqual([
+    {
+      id: events[0].id,
+      message: createNotificationMessage(events[0]),
+    },
+  ]);
+});
