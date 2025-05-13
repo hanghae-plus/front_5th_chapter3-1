dc359c94c90dd6b69c8b028577777fb7021b47d7
chloekim <chloe@perfitt.io>
2025-05-13 18:27:08 +0900

    createNotificationMessage 테스트 코드 추가

--- src/__tests__/unit/easy.notificationUtils.spec.ts
+++ src/__tests__/unit/easy.notificationUtils.spec.ts
@@ -1,6 +1,6 @@
+import eventJson from '../../__mocks__/response/events.json';
 import { Event } from '../../types';
-import { createNotificationMessage, getUpcomingEvents } from '../../utils/notificationUtils';
-
+import { createNotificationMessage } from '../../utils/notificationUtils';
 describe('getUpcomingEvents', () => {
   it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {});
 
@@ -12,5 +12,9 @@ describe('getUpcomingEvents', () => {
 });
 
 describe('createNotificationMessage', () => {
-  it('올바른 알림 메시지를 생성해야 한다', () => {});
+  it('올바른 알림 메시지를 생성해야 한다', () => {
+    const mockEvent = eventJson.events as Event[];
+    const result = createNotificationMessage(mockEvent[0]);
+    expect(result).toBe('10분 후 기존 회의 일정이 시작됩니다.');
+  });
 });
