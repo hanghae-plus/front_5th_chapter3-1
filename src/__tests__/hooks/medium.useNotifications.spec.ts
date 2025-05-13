07ad844373b50d4661ff8fe8e5f6eb9a2cda046c
hanbeulYou <hanbeul.you@gmail.com>
2025-05-14 02:03:28 +0900

    test: 알림 생성 로직 테스트 추가

--- src/__tests__/hooks/medium.useNotifications.spec.ts
+++ src/__tests__/hooks/medium.useNotifications.spec.ts
@@ -1,8 +1,9 @@
 import { act, renderHook } from '@testing-library/react';
 
+import eventsData from '../../__mocks__/response/events.json';
 import { useNotifications } from '../../hooks/useNotifications.ts';
 import { Event } from '../../types.ts';
-import { formatDate } from '../../utils/dateUtils.ts';
+import { createNotificationMessage } from '../../utils/notificationUtils.ts';
 import { parseHM } from '../utils.ts';
 
 it('초기 상태에서는 알림이 없어야 한다', () => {
@@ -10,7 +11,25 @@ it('초기 상태에서는 알림이 없어야 한다', () => {
   expect(result.current.notifications).toEqual([]);
 });
 
-it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', () => {});
+it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', async () => {
+  vi.useFakeTimers();
+  vi.setSystemTime('2025-07-01T08:50:00');
+
+  const events = [...eventsData.events] as Event[];
+  const { result } = renderHook(() => useNotifications(events));
+  expect(result.current.notifications).toEqual([]);
+
+  await act(async () => {
+    vi.advanceTimersByTime(1000);
+  });
+
+  expect(result.current.notifications).toEqual([
+    {
+      id: events[0].id,
+      message: createNotificationMessage(events[0]),
+    },
+  ]);
+});
 
 it('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {});
 
