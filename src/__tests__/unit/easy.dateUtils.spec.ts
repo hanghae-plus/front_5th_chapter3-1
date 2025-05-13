acac22510e2b1130e2dee74b6792a12baeb82e5d
chloekim <chloe@perfitt.io>
2025-05-13 15:20:03 +0900

    formatMonth 테스트 코드 추가

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -4,6 +4,7 @@ import {
   areDateArraysEqual,
   fillZero,
   formatDate,
+  formatMonth,
   formatWeek,
   getDaysInMonth,
   getEventsForDay,
@@ -214,7 +215,11 @@ describe('formatWeek', () => {
 });
 
 describe('formatMonth', () => {
-  it("2025년 7월 10일을 '2025년 7월'로 반환한다", () => {});
+  it("2025년 7월 10일을 '2025년 7월'로 반환한다", () => {
+    const date = new Date('2025-07-10');
+    const result = formatMonth(date);
+    expect(result).toBe('2025년 7월');
+  });
 });
 
 describe('isDateInRange', () => {
