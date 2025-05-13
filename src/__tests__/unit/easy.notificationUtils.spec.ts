c03faa7d7f6bc190955b6bfa62ec5a82667ac91a
Jin Kwan Kim <kkole3897@gmail.com>
2025-05-13 23:29:28 +0900

    test: createNotificationMessage 테스트 추가

--- src/__tests__/unit/easy.notificationUtils.spec.ts
+++ src/__tests__/unit/easy.notificationUtils.spec.ts
@@ -197,5 +197,25 @@ describe('getUpcomingEvents', () => {
 });
 
 describe('createNotificationMessage', () => {
-  it('올바른 알림 메시지를 생성해야 한다', () => {});
+  it('올바른 알림 메시지를 생성해야 한다', () => {
+    const event: Event = {
+      id: '1',
+      title: '이벤트 1',
+      date: '2025-07-01',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: '이벤트 1 설명',
+      location: '이벤트 1 장소',
+      category: '이벤트 1 카테고리',
+      repeat: {
+        type: 'none',
+        interval: 0,
+      },
+      notificationTime: 10,
+    };
+
+    const message = createNotificationMessage(event);
+
+    expect(message).toBe('10분 후 이벤트 1 일정이 시작됩니다.');
+  });
 });
