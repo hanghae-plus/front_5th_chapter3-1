e81135a8cf9bee308e866b7238d913144dcc8680
ssp <qkrtjdtlr02@naver.com>
2025-05-13 11:00:19 +0900

    handler 작성 및 useEventOperation 테스트 작성 완료

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,16 +1,52 @@
 import { http, HttpResponse } from 'msw';
 
 import { Event } from '../types';
+import {
+  setupMockHandlerFetch,
+  setupMockHandlerCreation,
+  setupMockHandlerUpdating,
+  setupMockHandlerDeletion,
+  setupMockHandlerAdding,
+} from './handlersUtils';
 import { events } from './response/events.json' assert { type: 'json' };
 
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
+setupMockHandlerCreation(events as Event[]);
+
 export const handlers = [
-  http.get('/api/events', () => {}),
+  http.get('/api/events', () => {
+    return HttpResponse.json({
+      events: setupMockHandlerFetch(),
+    });
+  }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  http.post('/api/events', async ({ request }) => {
+    const { ...rest } = (await request.json()) as Event;
 
-  http.put('/api/events/:id', async () => {}),
+    setupMockHandlerAdding([rest]);
+    return HttpResponse.json({
+      events: [...events, rest],
+    });
+  }),
 
-  http.delete('/api/events/:id', ({ params }) => {}),
+  http.put('/api/events/:id', async ({ request }) => {
+    const { id, ...rest } = (await request.json()) as Event;
+    const updateEvents = events.map((event) => (event.id === id ? { ...event, ...rest } : event));
+
+    setupMockHandlerUpdating(id, rest as Event);
+    return HttpResponse.json({
+      events: updateEvents,
+    });
+  }),
+
+  http.delete('/api/events/:id', ({ params }) => {
+    const { id } = params as { id: string };
+    const deletedEvents = events.filter((event) => event.id !== id) as Event[];
+
+    setupMockHandlerDeletion(id);
+    return HttpResponse.json({
+      events: deletedEvents,
+    });
+  }),
 ];
