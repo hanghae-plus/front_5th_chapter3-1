6bc9e10fe6d6fd27955df13038a9862d8e171a88
Doh Kim <ehgud456456@naver.com>
2025-05-13 17:30:22 +0900

    feat: 네트워크 오류로 삭제 실패하는 테스트

--- src/__tests__/hooks/medium.useEventOperations.spec.ts
+++ src/__tests__/hooks/medium.useEventOperations.spec.ts
@@ -165,4 +165,22 @@ it("존재하지 않는 이벤트 수정 시 '일정 저장 실패'라는 토스
   expect(result.current.events).toEqual(events);
 });
 
-it("네트워크 오류 시 '일정 삭제 실패'라는 텍스트가 노출되며 이벤트 삭제가 실패해야 한다", async () => {});
+it("네트워크 오류 시 '일정 삭제 실패'라는 텍스트가 노출되며 이벤트 삭제가 실패해야 한다", async () => {
+  server.use(http.delete('/api/events/:id', () => HttpResponse.error()));
+  const { result } = renderHook(() => useEventOperations(false));
+
+  act(() => {
+    result.current.deleteEvent(events[0].id);
+  });
+
+  await waitFor(() => {
+    expect(mockToast).toHaveBeenCalledWith({
+      title: '일정 삭제 실패',
+      status: 'error',
+      duration: 3000,
+      isClosable: true,
+    });
+  });
+
+  expect(result.current.events).toEqual(events);
+});
