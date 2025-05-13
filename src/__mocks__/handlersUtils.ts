ea4857b5b227cf85380663fb1f0a1a8982e9fc81
anveloper <hitedin@gmail.com>
2025-05-14 06:27:27 +0900

    ✅ UseEventOperation 구현, 통과 코드 작성완료, 에러 테스트 구현중

--- src/__mocks__/handlersUtils.ts
+++ src/__mocks__/handlersUtils.ts
@@ -1,49 +1,28 @@
 import { http, HttpResponse } from 'msw';
 
-import { server } from '../setupTests';
 import { Event } from '../types';
 
 // ! Hard
 // ! 이벤트는 생성, 수정 되면 fetch를 다시 해 상태를 업데이트 합니다. 이를 위한 제어가 필요할 것 같은데요. 어떻게 작성해야 테스트가 병렬로 돌아도 안정적이게 동작할까요?
 // ! 아래 이름을 사용하지 않아도 되니, 독립적이게 테스트를 구동할 수 있는 방법을 찾아보세요. 그리고 이 로직을 PR에 설명해주세요.
-let eventStore = [] as Event[];
+
 export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {
   const events: Event[] = [...initEvents];
 
-  server.use(
-    http.get('/api/events', () => {
-      return HttpResponse.json({ events });
-    }),
+  return [
+    http.get('/api/events', () => HttpResponse.json({ events })),
     http.post('/api/events', async ({ request }) => {
       const newEvent = (await request.json()) as Event;
       events.push({ ...newEvent, id: String(events.length + 1) });
       return HttpResponse.json(newEvent, { status: 201 });
-    })
-  );
-};
-const common: Event = {
-  id: '1',
-  title: '이벤트 1',
-  date: '2025-10-01',
-  startTime: '',
-  endTime: '',
-  description: '',
-  location: '',
-  category: '',
-  repeat: { type: 'none', interval: 0 },
-  notificationTime: 0,
-} as const;
-
-export const setupMockHandlerUpdating = () => {
-  const events: Event[] = [
-    { ...common, id: '1', title: '이벤트 1', startTime: '10:00', endTime: '11:00' },
-    { ...common, id: '2', title: '이벤트 2', startTime: '11:00', endTime: '12:00' },
-    { ...common, id: '3', title: '이벤트 3', startTime: '13:00', endTime: '14:00' },
-  ];
-  server.use(
-    http.get('/api/events', () => {
-      return HttpResponse.json({ events });
     }),
+  ];
+};
+
+export const setupMockHandlerUpdating = (initEvents = [] as Event[]) => {
+  const events: Event[] = [...initEvents];
+  return [
+    http.get('/api/events', () => HttpResponse.json({ events })),
     http.put('/api/events/:id', async ({ params, request }) => {
       const { id } = params;
       const updatedEvent = (await request.json()) as Event;
@@ -55,16 +34,14 @@ export const setupMockHandlerUpdating = () => {
       }
 
       return new HttpResponse(null, { status: 404 });
-    })
-  );
+    }),
+  ];
 };
 
-export const setupMockHandlerDeletion = () => {
-  const events: Event[] = [{ ...common }];
-  server.use(
-    http.get('/api/events', () => {
-      return HttpResponse.json({ events });
-    }),
+export const setupMockHandlerDeletion = (initEvents = [] as Event[]) => {
+  const events: Event[] = [...initEvents];
+  return [
+    http.get('/api/events', () => HttpResponse.json({ events })),
     http.delete('/api/events/:id', ({ params }) => {
       const { id } = params;
       const index = events.findIndex((event) => event.id === id);
@@ -75,6 +52,6 @@ export const setupMockHandlerDeletion = () => {
       }
 
       return new HttpResponse(null, { status: 404 });
-    })
-  );
+    }),
+  ];
 };
--- src/__tests__/hooks/medium.useEventOperations.spec.ts
+++ src/__tests__/hooks/medium.useEventOperations.spec.ts
@@ -1,22 +1,120 @@
-import { act, renderHook } from '@testing-library/react';
-import { http, HttpResponse } from 'msw';
+import { act, renderHook, waitFor } from '@testing-library/react';
 
 import {
   setupMockHandlerCreation,
   setupMockHandlerDeletion,
   setupMockHandlerUpdating,
 } from '../../__mocks__/handlersUtils.ts';
+import { events } from '../../__mocks__/response/events.json' assert { type: 'json' };
 import { useEventOperations } from '../../hooks/useEventOperations.ts';
 import { server } from '../../setupTests.ts';
 import { Event } from '../../types.ts';
 
-it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {});
+waitFor;
 
-it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {});
+const toastFn = vi.fn();
+vi.mock('@chakra-ui/react', async () => {
+  const actual = await vi.importActual('@chakra-ui/react');
+  return { ...actual, useToast: () => toastFn };
+});
 
-it("새로 정의된 'title', 'endTime' 기준으로 적절하게 일정이 업데이트 된다", async () => {});
+const flushPromise = () => Promise.resolve(); // waitFor 비동작
 
-it('존재하는 이벤트 삭제 시 에러없이 아이템이 삭제된다.', async () => {});
+it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {
+  const handlers = setupMockHandlerCreation(events as Event[]);
+  server.use(...handlers);
+  const { result } = renderHook(() => useEventOperations(false));
+
+  await act(flushPromise);
+  // await waitFor(() => {});
+  expect(result.current.events).toEqual(events);
+
+  expect(toastFn).toHaveBeenCalledWith({
+    title: '일정 로딩 완료!',
+    status: 'info',
+    duration: 1000,
+  });
+});
+
+it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {
+  const handlers = setupMockHandlerCreation(events as Event[]);
+  server.use(...handlers);
+  const { result } = renderHook(() => useEventOperations(false));
+
+  const newEvent: Event = {
+    id: '2',
+    title: '',
+    date: '',
+    startTime: '',
+    endTime: '',
+    description: '',
+    location: '',
+    category: '',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 0,
+  };
+
+  act(() => {
+    result.current.saveEvent(newEvent);
+  });
+
+  await act(flushPromise);
+  // await waitFor(() => {});
+  expect(result.current.events.length).toBe(2);
+  expect(result.current.events[1]).toEqual(newEvent);
+  expect(toastFn).toHaveBeenCalledWith({
+    title: '일정이 추가되었습니다.',
+    status: 'success',
+    duration: 3000,
+    isClosable: true,
+  });
+});
+
+it("새로 정의된 'title', 'endTime' 기준으로 적절하게 일정이 업데이트 된다", async () => {
+  const handler = setupMockHandlerUpdating(events as Event[]);
+  server.use(...handler);
+  const { result } = renderHook(() => useEventOperations(true));
+
+  const updatedEvent: Event = {
+    ...(events[0] as Event),
+    title: '수정',
+    endTime: ' 12:00',
+  };
+
+  act(() => {
+    result.current.saveEvent(updatedEvent);
+  });
+  // await waitFor(() => {});
+  await act(flushPromise);
+  expect(result.current.events[0]).toEqual(updatedEvent);
+
+  expect(toastFn).toHaveBeenCalledWith({
+    title: '일정이 수정되었습니다.',
+    status: 'success',
+    duration: 3000,
+    isClosable: true,
+  });
+});
+
+it('존재하는 이벤트 삭제 시 에러없이 아이템이 삭제된다.', async () => {
+  server.use(...setupMockHandlerDeletion(events as Event[]));
+  const { result } = renderHook(() => useEventOperations(false));
+
+  act(() => {
+    result.current.deleteEvent(events[0].id);
+  });
+
+  await act(flushPromise);
+  // await waitFor(() => {});
+  expect(result.current.events.length).toBe(0);
+
+  expect(toastFn).toHaveBeenCalledWith({
+    title: '일정이 삭제되었습니다.',
+    status: 'info',
+    duration: 3000,
+    isClosable: true,
+  });
+});
 
 it("이벤트 로딩 실패 시 '이벤트 로딩 실패'라는 텍스트와 함께 에러 토스트가 표시되어야 한다", async () => {});
 
