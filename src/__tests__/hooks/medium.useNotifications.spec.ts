80d32a885be2a64bee77756cd416c39a06291838
yoojinDev <yj.oh@teamsparta.co>
2025-05-14 11:53:55 +0900

    medium.useNotifications.spec.ts 테스트 작성 및 passed

--- src/__tests__/hooks/medium.useNotifications.spec.ts
+++ src/__tests__/hooks/medium.useNotifications.spec.ts
@@ -2,7 +2,8 @@ import { act, renderHook } from '@testing-library/react';
 
 import { useNotifications } from '../../hooks/useNotifications.ts';
 import { Event, RepeatType } from '../../types.ts';
-import { formatDate } from '../../utils/dateUtils.ts';
+import { formatDate, formatTime } from '../../utils/dateUtils.ts';
+import { createNotificationMessage } from '../../utils/notificationUtils.ts';
 import { parseHM } from '../utils.ts';
 
 function makeEvent(id: string, updatedProperty: Partial<Event> = {}): Event {
@@ -30,18 +31,72 @@ it('초기 상태에서는 알림이 없어야 한다', () => {
 });
 
 it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', () => {
-  const pastEvent: Event = makeEvent('1', {
-    date: '2025-01-01',
+  const upComingEvent: Event = makeEvent('1', {
+    date: formatDate(new Date()),
+    startTime: '01:30',
+    endTime: '02:10',
+    notificationTime: 120,
   });
-  const { result } = renderHook(() => useNotifications([pastEvent]));
+  const { result } = renderHook(() => useNotifications([upComingEvent]));
 
   vi.useFakeTimers();
   act(() => {
     vi.advanceTimersByTime(1000);
   });
-  console.log(result.current.notifications);
+
+  expect(result.current.notifications).toEqual([
+    {
+      id: upComingEvent.id,
+      message: createNotificationMessage(upComingEvent),
+    },
+  ]);
 });
 
-it('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {});
+it('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {
+  const upComingEvent: Event = makeEvent('1', {
+    date: formatDate(new Date()),
+    startTime: '01:30',
+    endTime: '02:10',
+    notificationTime: 120,
+  });
+  const { result } = renderHook(() => useNotifications([upComingEvent]));
 
-it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', () => {});
+  vi.useFakeTimers();
+  act(() => {
+    vi.advanceTimersByTime(1000);
+  });
+
+  act(() => {
+    result.current.removeNotification(0);
+  });
+
+  expect(result.current.notifications).toEqual([]);
+});
+
+it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', () => {
+  const upComingEvent: Event = makeEvent('1', {
+    date: formatDate(new Date()),
+    startTime: '01:30',
+    endTime: '02:10',
+    notificationTime: 120,
+  });
+  const { result } = renderHook(() => useNotifications([upComingEvent]));
+
+  vi.useFakeTimers();
+
+  act(() => {
+    vi.advanceTimersByTime(1000);
+  });
+
+  act(() => {
+    vi.advanceTimersByTime(1000);
+  });
+
+  //타이머가 두번 지나도 알림이 하나만 생성
+  expect(result.current.notifications).toEqual([
+    {
+      id: upComingEvent.id,
+      message: createNotificationMessage(upComingEvent),
+    },
+  ]);
+});
