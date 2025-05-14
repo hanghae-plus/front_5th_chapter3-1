81254462d7f51d00acf97dd7447bf09bf6c4d221
chloekim <chloe@perfitt.io>
2025-05-14 16:14:06 +0900

    handler 작성

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -6,15 +6,34 @@ import { events } from './response/events.json' assert { type: 'json' };
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
+  /**이벤트 목록 조회 */
   http.get('/api/events', () => {
     return HttpResponse.json(events);
   }),
-
+  /*새 이벤트 생성 (POST) */
   http.post('/api/events', async ({ request }) => {
     const { event } = (await request.json()) as { event: Event };
+    events.push(event);
+    return HttpResponse.json({ event });
   }),
 
-  http.put('/api/events/:id', async () => {}),
-
-  http.delete('/api/events/:id', ({ params }) => {}),
+  /**이벤트 수정 (PUT) */
+  http.put('/api/events/:id', async ({ request, params }) => {
+    const { id } = params;
+    const updatedEvent = (await request.json()) as Event;
+    const index = events.findIndex((event) => event.id === id);
+    if (index !== -1) {
+      events[index] = updatedEvent;
+    }
+    return HttpResponse.json({ success: true }, { status: 200 });
+  }),
+  /**이벤트 삭제 (DELETE) */
+  http.delete('/api/events/:id', ({ params }) => {
+    const { id } = params;
+    const index = events.findIndex((event) => event.id === id);
+    if (index !== -1) {
+      events.splice(index, 1);
+    }
+    return HttpResponse.json({ success: true }, { status: 200 });
+  }),
 ];
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,15 +1,17 @@
-import { setupServer } from 'msw/node';
 import '@testing-library/jest-dom';
+import { setupServer } from 'msw/node';
 
 import { handlers } from './__mocks__/handlers';
 
 /* msw */
 export const server = setupServer(...handlers);
 
+//mock 서버를 시작해 테스트 중의 네트워크 요청을 가로챔
 beforeAll(() => {
   server.listen();
 });
 
+//expect.hasAssertions()는 테스트 내에서 expect()를 최소 1번 이상 호출했는지 검사
 beforeEach(() => {
   expect.hasAssertions();
 });
