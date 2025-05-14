9a23ece7396b4f6dad02b58d83f662a2a0c64a09
KumJungMin <rmawjdals@naver.com>
2025-05-14 22:02:03 +0900

    chore: 선언한 변수를 검증할 때 사용하기

--- src/__tests__/hooks/easy.useSearch.spec.ts
+++ src/__tests__/hooks/easy.useSearch.spec.ts
@@ -21,24 +21,24 @@ describe('useSearch 훅', () => {
   it('검색어에 맞는 이벤트만 필터링해야 한다', () => {
     act(() => result.current.setSearchTerm('회의'));
 
-    expect(result.current.filteredEvents).toEqual([events[0]]);
+    expect(result.current.filteredEvents).toEqual([eventList[0]]);
   });
 
   it('검색어가 제목, 설명, 위치 중 하나라도 일치하면 해당 이벤트를 반환해야 한다', () => {
     act(() => result.current.setSearchTerm('헬스장'));
 
-    expect(result.current.filteredEvents).toEqual([events[4]]);
+    expect(result.current.filteredEvents).toEqual([eventList[4]]);
   });
 
   it('현재 뷰(주간/월간)에 해당하는 이벤트만 반환해야 한다', () => {
     act(() => result.current.setSearchTerm('회의'));
 
-    expect(result.current.filteredEvents).toEqual([events[0]]);
+    expect(result.current.filteredEvents).toEqual([eventList[0]]);
   });
 
   it("검색어를 '회의'에서 '점심'으로 변경하면 필터링된 결과가 즉시 업데이트되어야 한다", () => {
     act(() => result.current.setSearchTerm('점심'));
 
-    expect(result.current.filteredEvents).toEqual([events[1]]);
+    expect(result.current.filteredEvents).toEqual([eventList[1]]);
   });
 });
