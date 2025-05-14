e9915e40b880959b4d9deb54bb3c267b23269aa8
chloekim <chloe@perfitt.io>
2025-05-14 11:49:01 +0900

    notificationUtil쪽 테스트 코드 추가

--- src/__tests__/unit/easy.notificationUtils.spec.ts
+++ src/__tests__/unit/easy.notificationUtils.spec.ts
@@ -1,14 +1,53 @@
 import eventJson from '../../__mocks__/response/events.json';
 import { Event } from '../../types';
-import { createNotificationMessage } from '../../utils/notificationUtils';
+import { createNotificationMessage, getUpcomingEvents } from '../../utils/notificationUtils';
 describe('getUpcomingEvents', () => {
-  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {});
+  const baseEvent = {
+    id: 'test-event',
+    title: '테스트 이벤트',
+    date: '2025-07-01',
+    endTime: '11:00',
+    description: '',
+    location: '',
+    category: '',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 10,
+  } as Event;
 
-  it('이미 알림이 간 이벤트는 제외한다', () => {});
+  const now = new Date('2025-07-01T10:00:00'); //10시
 
-  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {});
+  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {
+    const events = [
+      {
+        ...baseEvent,
+        id: '1',
+        startTime: '10:10',
+      },
+    ];
 
-  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {});
+    const result = getUpcomingEvents(events, now, []);
+    expect(result.some((e) => e.id === '1')).toBe(true);
+  });
+
+  it('이미 알림이 간 이벤트는 제외한다', () => {
+    const events = [{ ...baseEvent, id: '3', startTime: '10:10' }];
+    const result = getUpcomingEvents(events, now, ['3']);
+
+    expect(result).toHaveLength(0);
+  });
+
+  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {
+    const events = [{ ...baseEvent, id: '5', startTime: '10:20' }];
+    const result = getUpcomingEvents(events, now, []);
+    expect(result).toHaveLength(0);
+  });
+
+  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {
+    const nowTime = new Date('2025-07-01T10:16');
+    const events = [{ ...baseEvent, id: '6', startTime: '10:04' }];
+    const result = getUpcomingEvents(events, nowTime, []);
+    expect(result).toHaveLength(0);
+  });
 });
 
 describe('createNotificationMessage', () => {
