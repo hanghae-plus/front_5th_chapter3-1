32fc4f546dbb6184e44420f6395c80944fb4e801
상은 <1-blue98@naver.com>
2025-05-14 22:57:17 +0900

    ♻️: 테스트 코드 타임존 이슈 처리

--- src/__tests__/unit/easy.eventUtils.spec.ts
+++ src/__tests__/unit/easy.eventUtils.spec.ts
@@ -2,7 +2,7 @@ import { getFilteredEvents } from '../../utils/eventUtils';
 import { makeEvents } from '../utils';
 
 describe('getFilteredEvents', () => {
-  const DEFAULT_DATE = new Date('2025-07-01');
+  const DEFAULT_DATE = new Date('2025-07-01T00:00:00Z');
 
   it("검색어 '이벤트 2'에 맞는 이벤트만 반환한다", () => {
     const events = makeEvents(24).map((event, index) => ({
@@ -22,7 +22,7 @@ describe('getFilteredEvents', () => {
     // 2025-06-28 ~ 2025-07-07
     const events = makeEvents(10).map((event, index) => ({
       ...event,
-      date: new Date(DEFAULT_DATE.getFullYear(), DEFAULT_DATE.getMonth(), index - 1)
+      date: new Date(DEFAULT_DATE.getFullYear(), DEFAULT_DATE.getMonth(), index - 1, 9)
         .toISOString()
         .split('T')[0],
     }));
@@ -30,23 +30,22 @@ describe('getFilteredEvents', () => {
     const filteredEvents = getFilteredEvents(events, '', DEFAULT_DATE, 'week');
 
     // 2025-06-29(일) ~ 2025-07-05(토)
-    expect(filteredEvents).toStrictEqual(events.slice(1, 8));
+    expect(filteredEvents).toStrictEqual(events.slice(0, 7));
   });
 
   it('월간 뷰에서 2025년 7월의 모든 이벤트를 반환한다', () => {
     // 2025-06-28 ~ 2025-08-06
     const events = makeEvents(40).map((event, index) => ({
       ...event,
-      date: new Date(DEFAULT_DATE.getFullYear(), DEFAULT_DATE.getMonth(), index - 1)
+      date: new Date(DEFAULT_DATE.getFullYear(), DEFAULT_DATE.getMonth(), index - 1, 9)
         .toISOString()
         .split('T')[0],
     }));
 
     const filteredEvents = getFilteredEvents(events, '', DEFAULT_DATE, 'month');
 
-    // FIXME: 시간 부족해서 일단 패스
-    // 2025-07-01 ~ 2025-07-30 ( 31일은 왜 필터링 안되는지 정확히 모르겠지만 시간이 15시로 되는거랑 연관이 있을 것 같은데 )
-    expect(filteredEvents).toStrictEqual(events.slice(3, 33));
+    // 2025-07-01 ~ 2025-07-31
+    expect(filteredEvents).toStrictEqual(events.slice(2, 33));
   });
 
   it("검색어 '이벤트'와 주간 뷰 필터링을 동시에 적용한다", () => {
@@ -86,18 +85,18 @@ describe('getFilteredEvents', () => {
   });
 
   it('월의 경계에 있는 이벤트를 올바르게 필터링한다', () => {
-    // [2025-06-30, 2025-07-01]
+    // [2025-07-01, 2025-07-02]
     const events = makeEvents(2).map((event, index) => ({
       ...event,
       title: `이벤트 ${index + 1}`,
-      date: new Date(DEFAULT_DATE.getFullYear(), DEFAULT_DATE.getMonth(), index + 1)
+      date: new Date(DEFAULT_DATE.getFullYear(), DEFAULT_DATE.getMonth(), index + 1, 9) // 한국 시간이랑 안맞아서 +9시간
         .toISOString()
         .split('T')[0],
     }));
 
     const filteredEvents = getFilteredEvents(events, '', DEFAULT_DATE, 'month');
 
-    expect(filteredEvents).toStrictEqual(events.slice(1));
+    expect(filteredEvents).toStrictEqual(events.slice(0));
   });
 
   it('빈 이벤트 리스트에 대해 빈 배열을 반환한다', () => {
--- src/utils/eventUtils.ts
+++ src/utils/eventUtils.ts
@@ -26,7 +26,7 @@ function filterEventsByDateRangeAtWeek(events: Event[], currentDate: Date) {
 
 function filterEventsByDateRangeAtMonth(events: Event[], currentDate: Date) {
   const monthStart = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
-  const monthEnd = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+  const monthEnd = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0, 9); // 한국 시간이랑 안맞아서 +9시간
   return filterEventsByDateRange(events, monthStart, monthEnd);
 }
 
