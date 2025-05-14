d0222ba27a8f79bc2ae6817c093076cd568996d6
SeongYoonMin <namo4862@naver.com>
2025-05-15 02:45:01 +0900

    fix: mockEvent 값 받아오는 부분 수정
    특정한 값을 저장하고 제거하고 할때 handler안에서 mock데이터를 구성하면 테스터가 원하는 방식으로 테스트 불가능

--- src/__mocks__/handlersUtils.ts
+++ src/__mocks__/handlersUtils.ts
@@ -19,33 +19,9 @@ export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {
   );
 };
 
-export const setupMockHandlerUpdating = () => {
-  const mockEvents: Event[] = [
-    {
-      id: '1',
-      title: '기존 회의',
-      date: '2025-10-15',
-      startTime: '09:00',
-      endTime: '10:00',
-      description: '기존 팀 미팅',
-      location: '회의실 B',
-      category: '업무',
-      repeat: { type: 'none', interval: 0 },
-      notificationTime: 10,
-    },
-    {
-      id: '2',
-      title: '기존 회의2',
-      date: '2025-10-15',
-      startTime: '11:00',
-      endTime: '12:00',
-      description: '기존 팀 미팅 2',
-      location: '회의실 C',
-      category: '업무 회의',
-      repeat: { type: 'none', interval: 0 },
-      notificationTime: 5,
-    },
-  ];
+export const setupMockHandlerUpdating = (mockEvents: Event[]) => {
+  // mockEvent를 여기서 선언하고 수정하게되면, 어느것을 수정하든 계속 해당 선언이벤트만 수정하는 결과를 낳게됨.
+  // mockEvent를 직접 props로 받아서 수정하게해야한다.
 
   server.use(
     http.get('/api/events', () => {
@@ -62,21 +38,8 @@ export const setupMockHandlerUpdating = () => {
   );
 };
 
-export const setupMockHandlerDeletion = () => {
-  const mockEvents: Event[] = [
-    {
-      id: '1',
-      title: '삭제할 이벤트',
-      date: '2025-10-15',
-      startTime: '09:00',
-      endTime: '10:00',
-      description: '삭제할 이벤트입니다',
-      location: '어딘가',
-      category: '기타',
-      repeat: { type: 'none', interval: 0 },
-      notificationTime: 10,
-    },
-  ];
+export const setupMockHandlerDeletion = (mockEvents: Event[]) => {
+  
 
   server.use(
     http.get('/api/events', () => {
