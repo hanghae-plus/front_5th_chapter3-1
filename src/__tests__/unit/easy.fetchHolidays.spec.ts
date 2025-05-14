9cbc5d14158a9604e4fc0b8812f2e6a75e92fea6
상은 <1-blue98@naver.com>
2025-05-14 22:24:41 +0900

    ✅:[easy.fetchHolidays.spec] 공휴일 유틸 함수 테스트 코드 구현

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -1,9 +1,28 @@
 import { fetchHolidays } from '../../apis/fetchHolidays';
 
-describe.skip('fetchHolidays', () => {
-  it('주어진 월의 공휴일만 반환한다', () => {});
+describe('fetchHolidays', () => {
+  it('주어진 월의 공휴일만 반환한다', () => {
+    const holidays = fetchHolidays(new Date('2025-06-01'));
 
-  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {});
+    expect(holidays).toStrictEqual({
+      '2025-06-06': '현충일',
+    });
+  });
 
-  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {});
+  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {
+    const holidays = fetchHolidays(new Date('2025-07-01'));
+
+    expect(holidays).toStrictEqual({});
+  });
+
+  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    const holidays = fetchHolidays(new Date('2025-01-01'));
+
+    expect(holidays).toStrictEqual({
+      '2025-01-01': '신정',
+      '2025-01-29': '설날',
+      '2025-01-30': '설날',
+      '2025-01-31': '설날',
+    });
+  });
 });
