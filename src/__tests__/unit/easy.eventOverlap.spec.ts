dd5b8e8b9d1879cc4c27672bb2155481b4504d5c
서주희 <maywngml@naver.com>
2025-05-14 23:00:24 +0900

    기댓값에 변수는 넣지 않도록 변경, 이벤트 데이터는 각 테스트 케이스에서 새로 정의하도록 수정

--- src/__tests__/unit/easy.eventOverlap.spec.ts
+++ src/__tests__/unit/easy.eventOverlap.spec.ts
@@ -9,11 +9,9 @@ import {
 
 describe('parseDateTime', () => {
   it('2025-07-01 14:30을 정확한 Date 객체로 변환한다', () => {
-    const date = '2025-07-01';
-    const time = '14:30';
-    const parsedDate = parseDateTime(date, time);
+    const parsedDate = parseDateTime('2025-07-01', '14:30');
 
-    expect(parsedDate.toISOString()).toBe(new Date(`${date}T${time}`).toISOString());
+    expect(parsedDate.toISOString()).toBe(new Date('2025-07-01T$14:30').toISOString());
   });
 
   it("날짜 형식이 'YYYY', 'YYYY-MM', 'YYYY-MM-DD'가 아닐 경우 Invalid Date를 반환한다", () => {
@@ -36,64 +34,167 @@ describe('parseDateTime', () => {
 
 describe('convertEventToDateRange', () => {
   it('일반적인 이벤트를 올바른 시작 및 종료 시간을 가진 객체로 변환한다', () => {
-    const event = realEvents.events[0] as Event;
-    const { date, startTime, endTime } = event;
+    const event: Event = {
+      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+      title: '팀 회의',
+      date: '2025-05-20',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: '주간 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    };
     const convertedDateRange = convertEventToDateRange(event);
 
-    expect(convertedDateRange.start.toISOString()).toBe(
-      new Date(`${date}T${startTime}`).toISOString()
-    );
-    expect(convertedDateRange.end.toISOString()).toBe(new Date(`${date}T${endTime}`).toISOString());
+    expect(convertedDateRange.start.toISOString()).toBe(new Date('2025-05-20T10:00').toISOString());
+    expect(convertedDateRange.end.toISOString()).toBe(new Date('2025-05-20T11:00').toISOString());
   });
 
   it("이벤트 날짜 형식이 'YYYY', 'YYYY-MM', 'YYYY-MM-DD'가 아닌 경우 시작과 종료 시간이 Invalid Date인 객체로 변환한다", () => {
-    const event = realEvents.events[0] as Event;
-    const date = '2025-001';
-    const convertedDateRange = convertEventToDateRange({ ...event, date });
+    const event: Event = {
+      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+      title: '팀 회의',
+      date: '2025-001',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: '주간 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    };
+    const convertedDateRange = convertEventToDateRange(event);
 
     expect(convertedDateRange.start).toEqual(new Date(NaN));
     expect(convertedDateRange.end).toEqual(new Date(NaN));
   });
 
   it("이벤트 시작 시간 형식이 'hh:mm'나 'hh:mm:ss'가 아닌 경우 시작 시간이 Invalid Date인 객체로 변환한다", () => {
-    const event = realEvents.events[0] as Event;
-    const startTime = '12';
-    const { date, endTime } = event;
-    const convertedDateRange = convertEventToDateRange({ ...event, startTime });
+    const event: Event = {
+      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+      title: '팀 회의',
+      date: '2025-01-01',
+      startTime: '12',
+      endTime: '11:00',
+      description: '주간 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    };
+    const convertedDateRange = convertEventToDateRange(event);
 
     expect(convertedDateRange.start).toEqual(new Date(NaN));
-    expect(convertedDateRange.end.toISOString()).toBe(new Date(`${date}T${endTime}`).toISOString());
+    expect(convertedDateRange.end.toISOString()).toBe(new Date('2025-01-01T11:00').toISOString());
   });
   it("이벤트 종료 시간 형식이 'hh:mm'나 'hh:mm:ss'가 아닌 경우 종료 시간이 Invalid Date인 객체로 변환한다", () => {
-    const event = realEvents.events[0] as Event;
-    const endTime = '14';
-    const { date, startTime } = event;
-    const convertedDateRange = convertEventToDateRange({ ...event, endTime });
+    const event: Event = {
+      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+      title: '팀 회의',
+      date: '2025-01-01',
+      startTime: '11:00',
+      endTime: '14',
+      description: '주간 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    };
+    const convertedDateRange = convertEventToDateRange(event);
 
-    expect(convertedDateRange.start.toISOString()).toBe(
-      new Date(`${date}T${startTime}`).toISOString()
-    );
+    expect(convertedDateRange.start.toISOString()).toBe(new Date('2025-01-01T11:00').toISOString());
     expect(convertedDateRange.end).toEqual(new Date(NaN));
   });
 });
 
 describe('isOverlapping', () => {
   it('한 이벤트가 다른 이벤트의 시간 범위에 완전히 포함되면 true를 반환한다', () => {
-    const firstEvent = { ...realEvents.events[0], startTime: '11:30', endTime: '15:00' } as Event;
-    const secondEvent = { ...realEvents.events[0], startTime: '12:30', endTime: '14:00' } as Event;
+    const firstEvent: Event = {
+      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+      title: '팀 회의',
+      date: '2025-01-01',
+      startTime: '11:30',
+      endTime: '15:00',
+      description: '주간 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    };
+    const secondEvent: Event = {
+      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+      title: '팀 회의',
+      date: '2025-01-01',
+      startTime: '12:30',
+      endTime: '14:00',
+      description: '주간 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    };
 
     expect(isOverlapping(firstEvent, secondEvent)).toBeTruthy();
   });
 
   it('두 이벤트의 시간이 겹치기만 해도 true를 반환한다', () => {
-    const firstEvent = { ...realEvents.events[0], startTime: '11:30', endTime: '15:00' } as Event;
-    const secondEvent = { ...realEvents.events[0], startTime: '13:30', endTime: '14:00' } as Event;
+    const firstEvent: Event = {
+      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+      title: '팀 회의',
+      date: '2025-01-01',
+      startTime: '11:30',
+      endTime: '15:00',
+      description: '주간 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    };
+    const secondEvent: Event = {
+      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+      title: '팀 회의',
+      date: '2025-01-01',
+      startTime: '13:30',
+      endTime: '16:00',
+      description: '주간 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    };
 
     expect(isOverlapping(firstEvent, secondEvent)).toBeTruthy();
   });
 
   it('두 이벤트의 시간이 겹치지 않는 경우 false를 반환한다', () => {
-    expect(isOverlapping(realEvents.events[0] as Event, realEvents.events[1] as Event)).toBeFalsy();
+    const firstEvent: Event = {
+      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+      title: '팀 회의',
+      date: '2025-01-01',
+      startTime: '11:30',
+      endTime: '15:00',
+      description: '주간 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    };
+    const secondEvent: Event = {
+      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+      title: '팀 회의',
+      date: '2025-02-01',
+      startTime: '13:30',
+      endTime: '16:00',
+      description: '주간 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    };
+
+    expect(isOverlapping(firstEvent, secondEvent)).toBeFalsy();
   });
 });
 
