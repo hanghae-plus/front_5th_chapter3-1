cbc78f0eba4cd230d4bb879bccd24cb54f1d518a
서주희 <maywngml@naver.com>
2025-05-14 18:25:31 +0900

    parseDateTime Date 객체 변환 확인하는 테스트 코드 수정, convertEventToDateRange 테스트 코드 구현 및 기존 테스트 설명 수정

--- src/__tests__/unit/easy.eventOverlap.spec.ts
+++ src/__tests__/unit/easy.eventOverlap.spec.ts
@@ -1,4 +1,5 @@
-import { Event } from '../../types';
+import { dummyEvents } from '../../__mocks__/events';
+import type { Event } from '../../types';
 import {
   convertEventToDateRange,
   findOverlappingEvents,
@@ -8,16 +9,14 @@ import {
 
 describe('parseDateTime', () => {
   it('2025-07-01 14:30을 정확한 Date 객체로 변환한다', () => {
-    const parsedDate = parseDateTime('2025-07-01', '14:30');
+    const date = '2025-07-01';
+    const time = '14:30';
+    const parsedDate = parseDateTime(date, time);
 
-    expect(parsedDate.getFullYear()).toBe(2025);
-    expect(parsedDate.getMonth()).toBe(6);
-    expect(parsedDate.getDate()).toBe(1);
-    expect(parsedDate.getHours()).toBe(14);
-    expect(parsedDate.getMinutes()).toBe(30);
+    expect(parsedDate.toISOString()).toBe(new Date(`${date}T${time}`).toISOString());
   });
 
-  it("날짜 형식이 'YYYY-MM-DD'가 아닐 경우 Invalid Date를 반환한다", () => {
+  it("날짜 형식이 'YYYY', 'YYYY-MM', 'YYYY-MM-DD'가 아닐 경우 Invalid Date를 반환한다", () => {
     expect(parseDateTime('20-212', '14:30')).toEqual(new Date(NaN));
   });
 
@@ -36,11 +35,46 @@ describe('parseDateTime', () => {
 });
 
 describe('convertEventToDateRange', () => {
-  it('일반적인 이벤트를 올바른 시작 및 종료 시간을 가진 객체로 변환한다', () => {});
+  it('일반적인 이벤트를 올바른 시작 및 종료 시간을 가진 객체로 변환한다', () => {
+    const event = dummyEvents[0];
+    const { date, startTime, endTime } = event;
+    const convertedDateRange = convertEventToDateRange(event);
 
-  it('잘못된 날짜 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {});
+    expect(convertedDateRange.start.toISOString()).toBe(
+      new Date(`${date}T${startTime}`).toISOString()
+    );
+    expect(convertedDateRange.end.toISOString()).toBe(new Date(`${date}T${endTime}`).toISOString());
+  });
 
-  it('잘못된 시간 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {});
+  it("이벤트 날짜 형식이 'YYYY', 'YYYY-MM', 'YYYY-MM-DD'가 아닌 경우 시작과 종료 시간이 Invalid Date인 객체로 변환한다", () => {
+    const event = dummyEvents[0];
+    const date = '2025-001';
+    const convertedDateRange = convertEventToDateRange({ ...event, date });
+
+    expect(convertedDateRange.start).toEqual(new Date(NaN));
+    expect(convertedDateRange.end).toEqual(new Date(NaN));
+  });
+
+  it("이벤트 시작 시간 형식이 'hh:mm'나 'hh:mm:ss'가 아닌 경우 시작 시간이 Invalid Date인 객체로 변환한다", () => {
+    const event = dummyEvents[0];
+    const startTime = '12';
+    const { date, endTime } = event;
+    const convertedDateRange = convertEventToDateRange({ ...event, startTime });
+
+    expect(convertedDateRange.start).toEqual(new Date(NaN));
+    expect(convertedDateRange.end.toISOString()).toBe(new Date(`${date}T${endTime}`).toISOString());
+  });
+  it("이벤트 종료 시간 형식이 'hh:mm'나 'hh:mm:ss'가 아닌 경우 종료 시간이 Invalid Date인 객체로 변환한다", () => {
+    const event = dummyEvents[0];
+    const endTime = '14';
+    const { date, startTime } = event;
+    const convertedDateRange = convertEventToDateRange({ ...event, endTime });
+
+    expect(convertedDateRange.start.toISOString()).toBe(
+      new Date(`${date}T${startTime}`).toISOString()
+    );
+    expect(convertedDateRange.end).toEqual(new Date(NaN));
+  });
 });
 
 describe('isOverlapping', () => {
