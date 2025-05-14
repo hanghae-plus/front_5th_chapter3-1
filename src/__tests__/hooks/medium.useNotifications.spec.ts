f3d5dc9e2566f3dab55333a58b186cf27ab4f7ce
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-05-14 21:08:29 +0900

    feat: medium.useNotifications 테스트 추가

--- src/__tests__/hooks/medium.useNotifications.spec.ts
+++ src/__tests__/hooks/medium.useNotifications.spec.ts
@@ -2,13 +2,100 @@ import { act, renderHook } from '@testing-library/react';
 
 import { useNotifications } from '../../hooks/useNotifications.ts';
 import { Event } from '../../types.ts';
-import { formatDate } from '../../utils/dateUtils.ts';
-import { parseHM } from '../utils.ts';
+import { createNotificationMessage } from '../../utils/notificationUtils.ts';
 
-it('초기 상태에서는 알림이 없어야 한다', () => {});
+const events: Event[] = [
+  {
+    id: '1',
+    title: '이벤트 1',
+    date: '2025-10-01',
+    startTime: '10:01',
+    endTime: '11:00',
+    description: '회의 설명',
+    location: '회의실 A',
+    category: '업무',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 1,
+  },
+];
 
-it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', () => {});
+describe('useNotifications', () => {
+  beforeEach(() => {
+    vi.useRealTimers();
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date('2025-10-01T10:00:00'));
+  });
+  afterEach(() => {
+    vi.useRealTimers();
+  });
 
-it('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {});
+  it('초기 상태에서는 알림이 없어야 한다', () => {
+    const { result } = renderHook(() => useNotifications(events));
+    expect(result.current.notifications).toEqual([]);
+  });
 
-it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', () => {});
+  it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', async () => {
+    const { result } = renderHook(() => useNotifications(events));
+    expect(result.current.notifications).toEqual([]);
+
+    await act(async () => {
+      vi.advanceTimersByTime(1000);
+    });
+
+    expect(result.current.notifications).toEqual([
+      {
+        id: events[0].id,
+        message: createNotificationMessage(events[0]),
+      },
+    ]);
+  });
+
+  it('index를 기준으로 알림을 적절하게 제거할 수 있다', async () => {
+    const { result } = renderHook(() => useNotifications(events));
+    expect(result.current.notifications).toEqual([]);
+
+    await act(async () => {
+      vi.advanceTimersByTime(1000);
+    });
+
+    expect(result.current.notifications).toEqual([
+      {
+        id: events[0].id,
+        message: createNotificationMessage(events[0]),
+      },
+    ]);
+
+    await act(async () => {
+      result.current.removeNotification(0);
+    });
+
+    expect(result.current.notifications).toEqual([]);
+  });
+
+  it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', async () => {
+    const { result } = renderHook(() => useNotifications(events));
+    expect(result.current.notifications).toEqual([]);
+
+    await act(async () => {
+      vi.advanceTimersByTime(1000);
+    });
+
+    expect(result.current.notifications).toEqual([
+      {
+        id: events[0].id,
+        message: createNotificationMessage(events[0]),
+      },
+    ]);
+
+    await act(async () => {
+      vi.advanceTimersByTime(1000);
+    });
+
+    expect(result.current.notifications).toEqual([
+      {
+        id: events[0].id,
+        message: createNotificationMessage(events[0]),
+      },
+    ]);
+  });
+});
