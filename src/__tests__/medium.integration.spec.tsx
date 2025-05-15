cd964a7caf5a67d0ed622919e80bf937d4d3c938
SeongYoonMin <namo4862@naver.com>
2025-05-16 02:10:59 +0900

    test: 일정 CRUD 및 기본 기능 잘못된 expect구현 수정

--- src/__tests__/medium.integration.spec.tsx
+++ src/__tests__/medium.integration.spec.tsx
@@ -1,5 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { render, screen, within, act } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor, within } from "@testing-library/react";
 import { UserEvent, userEvent } from "@testing-library/user-event";
 import { ReactElement } from "react";
 import {
@@ -31,19 +31,32 @@ const saveSchedule = async (
   await user.type(screen.getByLabelText("설명"), description);
   await user.type(screen.getByLabelText("위치"), location);
   await user.selectOptions(screen.getByLabelText("카테고리"), category);
+  await user.click(screen.getByTestId("event-submit-button"));
+};
+
+const updateSchedule = async (
+  user: UserEvent,
+  form: Omit<Event, "id" | "notificationTime" | "repeat">
+) => {
+  const { title } = form;
+
+  await user.type(screen.getByLabelText("제목"), title);
 
   await user.click(screen.getByTestId("event-submit-button"));
 };
 
+
+
 // ! HINT. "검색 결과가 없습니다"는 초기에 노출되는데요. 그럼 검증하고자 하는 액션이 실행되기 전에 검증해버리지 않을까요? 이 테스트를 신뢰성있게 만드려면 어떻게 할까요?
 describe("일정 CRUD 및 기본 기능", () => {
+
   // ! HINT. event를 추가 제거하고 저장하는 로직을 잘 살펴보고, 만약 그대로 구현한다면 어떤 문제가 있을 지 고민해보세요.
   it("입력한 새로운 일정 정보에 맞춰 모든 필드가 이벤트 리스트에 정확히 저장된다.", async () => {
-    const { user } = setup(<App />);
     setupMockHandlerCreation([]);
+    const { user } = setup(<App />);
     const newEvent: Omit<Event, "id" | "notificationTime" | "repeat"> = {
       title: "팀 회의",
-      date: "2025-10-15", // setupTest는 10월 1일로 초기화시키기때문에 10월달 회의를 추가해봄.
+      date: "2025-10-22", // setupTest는 10월 1일로 초기화시키기때문에 10월달 회의를 추가해봄.
       startTime: "09:00",
       endTime: "10:00",
       description: "팀 미팅",
@@ -51,97 +64,101 @@ describe("일정 CRUD 및 기본 기능", () => {
       category: "업무",
     };
 
-    await saveSchedule(user, newEvent); // userEvent를 사용해 일정을 추가하는 함수
+    await saveSchedule(user, newEvent);
 
-    expect(screen.getByLabelText("제목"), "팀 회의");
-    expect(screen.getByLabelText("날짜"), "2025-10-25");
-    expect(screen.getByLabelText("시작 시간"), "09:00");
-    expect(screen.getByLabelText("종료 시간"), "10:00");
-    expect(screen.getByLabelText("설명"), "팀 미팅");
-    expect(screen.getByLabelText("위치"), "회의실 A");
-    expect(screen.getByLabelText("카테고리"), "업무");
+
+    const eventList = screen.getByTestId("event-list")
+    const eventTitle = within(eventList).getByText("팀 회의")
+    expect(eventTitle).toBeInTheDocument();
   });
 
   it("기존 일정의 세부 정보를 수정하고 변경사항이 정확히 반영된다", async () => {
-    setup(<App />);
-    setupMockHandlerCreation([
-      {
-        id: "1",
-        title: "기존 회의",
-        date: "2025-10-15",
-        startTime: "09:00",
-        endTime: "10:00",
-        description: "기존 팀 미팅",
-        location: "회의실 B",
-        category: "업무",
-        repeat: { type: "none", interval: 0 },
-        notificationTime: 10,
-      },
-    ]);
-    expect(screen.getByLabelText("제목"), "기존 회의");
     const mockEvents: Event[] = [
       {
         id: "1",
-        title: "기존 회의",
-        date: "2025-10-15",
+        title: "팀 회의",
+        date: "2025-10-22", // setupTest는 10월 1일로 초기화시키기때문에 10월달 회의를 추가해봄.
         startTime: "09:00",
         endTime: "10:00",
-        description: "기존 팀 미팅",
-        location: "회의실 B",
+        description: "팀 미팅",
+        location: "회의실 A",
         category: "업무",
         repeat: { type: "none", interval: 0 },
         notificationTime: 10,
       },
       {
-        id: "2",
-        title: "기존 회의2",
-        date: "2025-10-15",
-        startTime: "11:00",
-        endTime: "12:00",
-        description: "기존 팀 미팅 2",
-        location: "회의실 C",
-        category: "업무 회의",
+        id: "1",
+        title: "팀 회의2",
+        date: "2025-10-22", // setupTest는 10월 1일로 초기화시키기때문에 10월달 회의를 추가해봄.
+        startTime: "09:00",
+        endTime: "10:00",
+        description: "팀 미팅",
+        location: "회의실 A",
+        category: "업무",
         repeat: { type: "none", interval: 0 },
-        notificationTime: 5,
+        notificationTime: 10,
       },
     ];
+    setupMockHandlerCreation([mockEvents[0]]);
+    const { user } = setup(<App />);
     setupMockHandlerUpdating(mockEvents);
-    expect(screen.getByLabelText("제목"), "기존 회의2");
+    const eventList = screen.getByTestId("event-list");
+    await waitFor(() => {
+      expect(within(eventList).queryByText('검색 결과가 없습니다')).not.toBeInTheDocument();
+    });
+    const editButton = within(eventList).getByLabelText("Edit event");
+    await user.click(editButton);
+    await updateSchedule(user, {
+      title: "팀 회의2",
+      date: "2025-10-22", // setupTest는 10월 1일로 초기화시키기때문에 10월달 회의를 추가해봄.
+      startTime: "09:00",
+      endTime: "10:00",
+      description: "팀 미팅",
+      location: "회의실 A",
+      category: "업무",
+    });
+    await waitFor(() => {
+      expect(within(screen.getByTestId("event-list")).getByText("팀 회의2")).toBeInTheDocument()
+    })
   });
 
   it("일정을 삭제하고 더 이상 조회되지 않는지 확인한다", async () => {
-    setup(<App />);
-    setupMockHandlerCreation([
-      {
-        id: "1",
-        title: "삭제할 이벤트",
-        date: "2025-10-15",
-        startTime: "09:00",
-        endTime: "10:00",
-        description: "삭제할 이벤트입니다",
-        location: "어딘가",
-        category: "기타",
-        repeat: { type: "none", interval: 0 },
-        notificationTime: 10,
-      },
-    ]);
-    expect(screen.getByLabelText("제목"), "삭제할 이벤트");
-    const mockEvents: Event[] = [
-      {
-        id: "1",
-        title: "삭제할 이벤트",
-        date: "2025-10-15",
-        startTime: "09:00",
-        endTime: "10:00",
-        description: "삭제할 이벤트입니다",
-        location: "어딘가",
-        category: "기타",
-        repeat: { type: "none", interval: 0 },
-        notificationTime: 10,
-      },
-    ];
-    setupMockHandlerDeletion(mockEvents);
-    expect(screen.getByLabelText("제목"), "");
+    const deleteEvents: Event[] = [{
+      id: "1",
+      title: "팀 회의",
+      date: "2025-10-22", // setupTest는 10월 1일로 초기화시키기때문에 10월달 회의를 추가해봄.
+      startTime: "09:00",
+      endTime: "10:00",
+      description: "팀 미팅",
+      location: "회의실 A",
+      category: "업무",
+      repeat: { type: "none", interval: 0 },
+      notificationTime: 10,
+    },
+    {
+      id: "1",
+      title: "팀 회의2",
+      date: "2025-10-22", // setupTest는 10월 1일로 초기화시키기때문에 10월달 회의를 추가해봄.
+      startTime: "09:00",
+      endTime: "10:00",
+      description: "팀 미팅",
+      location: "회의실 A",
+      category: "업무",
+      repeat: { type: "none", interval: 0 },
+      notificationTime: 10,
+    }]
+    setupMockHandlerCreation(deleteEvents);
+    const { user } = setup(<App />);
+    setupMockHandlerDeletion(deleteEvents);
+    const eventList = screen.getByTestId("event-list");
+    await waitFor(() => {
+      expect(within(eventList).queryByText('검색 결과가 없습니다')).not.toBeInTheDocument();
+    });
+    const deleteButton = within(eventList).getAllByLabelText("Delete event");
+    await user.click(deleteButton[0]);
+    await waitFor(() => {
+      expect(within(eventList).getAllByLabelText("Delete event").length).toBe(1)
+    })
   });
 });
 
@@ -162,7 +179,7 @@ describe("일정 뷰", () => {
 
     await user.selectOptions(screen.getByLabelText("view"), "week");
     const eventList = screen.getByTestId("event-list");
-    
+
     // setupTest에서 초기시간을 10월 1일로 해놨기때문에 15일자 이벤트는 조회되지 않는 모습이다.
     expect(within(eventList).getByText("검색 결과가 없습니다.")).toBeInTheDocument();
   });
@@ -183,7 +200,7 @@ describe("일정 뷰", () => {
 
     await user.selectOptions(screen.getByLabelText("view"), "week");
     const eventList = screen.getByTestId("event-list");
-    
+
     // setupTest에서 초기시간을 10월 1일로 해놨기때문에 15일자 이벤트는 조회되지 않는 모습이다.
     expect(within(eventList).getByText("팀 회의")).toBeInTheDocument();
   });
@@ -193,7 +210,7 @@ describe("일정 뷰", () => {
     setupMockHandlerCreation([]);
 
     const eventList = screen.getByTestId("event-list");
-    
+
     // view select의 초기값은 month이기때문에 바로 조회하는 모습.
     expect(within(eventList).getByText("검색 결과가 없습니다.")).toBeInTheDocument();
   });
@@ -213,7 +230,7 @@ describe("일정 뷰", () => {
     await saveSchedule(user, newEvent);
 
     const eventList = screen.getByTestId("event-list");
-    
+
     expect(within(eventList).getByText("팀 회의")).toBeInTheDocument();
   });
 
@@ -224,7 +241,7 @@ describe("일정 뷰", () => {
     setup(<App />);
     setupMockHandlerCreation([]);
     const monthView = screen.getByTestId("month-view");
-    
+
     expect(within(monthView).getByText("신정")).toBeInTheDocument();
   });
 });
@@ -302,9 +319,9 @@ describe("검색 기능", () => {
 });
 
 describe("일정 충돌", () => {
-  it("겹치는 시간에 새 일정을 추가할 때 경고가 표시된다", async () => {});
+  it("겹치는 시간에 새 일정을 추가할 때 경고가 표시된다", async () => { });
 
-  it("기존 일정의 시간을 수정하여 충돌이 발생하면 경고가 노출된다", async () => {});
+  it("기존 일정의 시간을 수정하여 충돌이 발생하면 경고가 노출된다", async () => { });
 });
 
-it("notificationTime을 10으로 하면 지정 시간 10분 전 알람 텍스트가 노출된다", async () => {});
+it("notificationTime을 10으로 하면 지정 시간 10분 전 알람 텍스트가 노출된다", async () => { });
