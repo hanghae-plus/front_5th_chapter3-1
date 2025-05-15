a6b9e4d31323727a827167bbcae7616193bc46e6
SeongYoonMin <namo4862@naver.com>
2025-05-16 02:10:29 +0900

    fix: 핸들러 유틸 테스트에 맞게 수정..?

--- src/__mocks__/handlersUtils.ts
+++ src/__mocks__/handlersUtils.ts
@@ -11,7 +11,9 @@ export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {
       return HttpResponse.json({ events: mockEvents });
     }),
     http.post('/api/events', async ({ request }) => {
-      const newEvent = (await request.json()) as Event;
+      const result = await request.json();
+      console.log('result', result);
+      const newEvent = result as Event;
       newEvent.id = String(mockEvents.length + 1); // 간단한 ID 생성
       mockEvents.push(newEvent);
       return HttpResponse.json(newEvent, { status: 201 });
@@ -19,19 +21,20 @@ export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {
   );
 };
 
-export const setupMockHandlerUpdating = (mockEvents: Event[]) => {
+export const setupMockHandlerUpdating = (mockEvents = [] as Event[]) => {
   // mockEvent를 여기서 선언하고 수정하게되면, 어느것을 수정하든 계속 해당 선언이벤트만 수정하는 결과를 낳게됨.
   // mockEvent를 직접 props로 받아서 수정하게해야한다.
-
+  const updateEvents: Event[] = [...mockEvents];
   server.use(
     http.get('/api/events', () => {
-      return HttpResponse.json({ events: mockEvents });
+
+      return HttpResponse.json({ events: [updateEvents[1]] });
     }),
     http.put('/api/events/:id', async ({ params, request }) => {
+      console.log("UPDATE")
       const { id } = params;
       const updatedEvent = (await request.json()) as Event;
       const index = mockEvents.findIndex((event) => event.id === id);
-
       mockEvents[index] = { ...mockEvents[index], ...updatedEvent };
       return HttpResponse.json(mockEvents[index]);
     })
@@ -39,11 +42,11 @@ export const setupMockHandlerUpdating = (mockEvents: Event[]) => {
 };
 
 export const setupMockHandlerDeletion = (mockEvents: Event[]) => {
-  
 
+  const deleteEvents = mockEvents.splice(0, 1);
   server.use(
     http.get('/api/events', () => {
-      return HttpResponse.json({ events: mockEvents });
+      return HttpResponse.json({ events: deleteEvents });
     }),
     http.delete('/api/events/:id', ({ params }) => {
       const { id } = params;
