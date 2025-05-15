a914596cc1f86c5c6f1b62f2838d37645fe0556b
hexdrinker <hexdrinker2020@gmail.com>
2025-05-15 16:07:48 +0900

    chore(constant): 상수 추출

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+const NOTIFICATION_OPTIONS = [
+  { value: 1, label: '1분 전' },
+  { value: 10, label: '10분 전' },
+  { value: 60, label: '1시간 전' },
+  { value: 120, label: '2시간 전' },
+  { value: 1440, label: '1일 전' },
+];
+
+const CATEGORIES = ['업무', '개인', '가족', '기타'];
+
+const WEEK_DAYS = ['일', '월', '화', '수', '목', '금', '토'];
+
+export { CATEGORIES, WEEK_DAYS, NOTIFICATION_OPTIONS };
