7d5335793a93793a89b286353c4a7d097ea39ec1
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-05-15 21:34:20 +0900

    Fix: handlerUtils

--- src/__mocks__/handlersUtils.ts
+++ src/__mocks__/handlersUtils.ts
@@ -6,10 +6,10 @@ import { Event } from '../types';
 // ! 이벤트는 생성, 수정 되면 fetch를 다시 해 상태를 업데이트 합니다. 이를 위한 제어가 필요할 것 같은데요. 어떻게 작성해야 테스트가 병렬로 돌아도 안정적이게 동작할까요?
 // ! 아래 이름을 사용하지 않아도 되니, 독립적이게 테스트를 구동할 수 있는 방법을 찾아보세요. 그리고 이 로직을 PR에 설명해주세요.
 
-export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {
-  const events = [...initEvents];
+export const createMockHandlers = (initEvents = [] as Event[]) => {
+  let events = [...initEvents];
 
-  server.use(
+  return [
     http.get('/api/events', () => {
       return HttpResponse.json({ events: events }, { status: 200 });
     }),
@@ -19,16 +19,6 @@ export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {
       const newEventId = (events.length + 1).toString();
       events.push({ ...newEvent, id: newEventId });
       return HttpResponse.json(newEvent, { status: 201 });
-    })
-  );
-};
-
-export const setupMockHandlerUpdating = (initEvents = [] as Event[]) => {
-  const events = [...initEvents];
-
-  server.use(
-    http.get('/api/events', () => {
-      return HttpResponse.json({ events: events }, { status: 200 });
     }),
 
     http.put('/api/events/:id', async ({ params, request }) => {
@@ -38,16 +28,6 @@ export const setupMockHandlerUpdating = (initEvents = [] as Event[]) => {
         return new HttpResponse(null, { status: 404 });
       }
       return HttpResponse.json(newEvent, { status: 201 });
-    })
-  );
-};
-
-export const setupMockHandlerDeletion = (initEvents = [] as Event[]) => {
-  const events = [...initEvents];
-
-  server.use(
-    http.get('/api/events', () => {
-      return HttpResponse.json({ events: events }, { status: 200 });
     }),
 
     http.delete('/api/events/:id', ({ params }) => {
@@ -57,6 +37,10 @@ export const setupMockHandlerDeletion = (initEvents = [] as Event[]) => {
       }
       events.splice(deleteIndex, 1);
       return HttpResponse.json(null, { status: 204 });
-    })
-  );
+    }),
+  ];
+};
+
+export const setupMockHandlers = (initEvents: Event[] = []) => {
+  server.use(...createMockHandlers(initEvents));
 };
