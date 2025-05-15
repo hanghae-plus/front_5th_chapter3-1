d7f4eec5a1b3659271f0b274254991863ea2e438
KumJungMin <rmawjdals@naver.com>
2025-05-15 22:12:23 +0900

    feat: 스케쥴폼에 컨텍스트 적용하기

--- src/components/schedule-form/ScheduleForm.tsx
+++ src/components/schedule-form/ScheduleForm.tsx
@@ -2,19 +2,23 @@ import { Button, Heading, VStack } from '@chakra-ui/react';
 
 import RepeatTimeForm from './RepeatTimeForm';
 import TimeRangeInput from './TimeRangeInput';
+import { useEventFormContext } from '../../contexts/EventFormContext';
+import { notificationOptions, categories } from '../../libs/constants';
 import CheckInput from '../../shares/ui/input/CheckInput';
 import LabelInput from '../../shares/ui/input/LabelInput';
 import Selector from '../../shares/ui/Selector';
-import { getTimeErrorMessage } from '../../utils/timeValidation';
 
-const ScheduleForm = (props) => {
+interface ScheduleFormProps {
+  editingEvent?: boolean;
+  addOrUpdateEvent: () => Promise<void>;
+}
+
+const ScheduleForm = ({ editingEvent = false, addOrUpdateEvent }: ScheduleFormProps) => {
   const {
     title,
     setTitle,
     date,
     setDate,
-    startTime,
-    endTime,
     description,
     setDescription,
     location,
@@ -23,46 +27,20 @@ const ScheduleForm = (props) => {
     setCategory,
     isRepeating,
     setIsRepeating,
-    repeatType,
-    setRepeatType,
-    repeatInterval,
-    setRepeatInterval,
-    repeatEndDate,
-    setRepeatEndDate,
     notificationTime,
     setNotificationTime,
-    categories,
-    notificationOptions,
-    editingEvent = false,
-    addOrUpdateEvent,
-    startTimeError,
-    handleStartTimeChange,
-    endTimeError,
-    handleEndTimeChange,
-  } = props;
+  } = useEventFormContext();
 
   return (
     <VStack w="400px" spacing={5} align="stretch">
       <Heading>{editingEvent ? '일정 수정' : '일정 추가'}</Heading>
 
       <LabelInput value={title} onChange={setTitle} label="제목" />
-
       <LabelInput value={date} onChange={setDate} label="날짜" type="date" />
 
-      <TimeRangeInput
-        startTime={startTime}
-        endTime={endTime}
-        startTimeTooltipLabel={startTimeError}
-        endTimeTooltipLabel={endTimeError}
-        isValidStartTime={!startTimeError}
-        isValidEndTime={!endTimeError}
-        onBlur={getTimeErrorMessage}
-        onChangeStartTime={handleStartTimeChange}
-        onChangeEndTime={handleEndTimeChange}
-      />
+      <TimeRangeInput />
 
       <LabelInput value={description} onChange={setDescription} label="설명" />
-
       <LabelInput value={location} onChange={setLocation} label="위치" />
 
       <Selector
@@ -84,19 +62,13 @@ const ScheduleForm = (props) => {
         label="알림 설정"
         value={notificationTime}
         onChange={(v) => setNotificationTime(Number(v))}
-        options={notificationOptions.map((item) => ({ value: item.value, label: item.label }))}
+        options={notificationOptions.map((option) => ({
+          value: option.value,
+          label: option.label,
+        }))}
       />
 
-      {isRepeating && (
-        <RepeatTimeForm
-          repeatType={repeatType}
-          setRepeatType={setRepeatType}
-          repeatInterval={repeatInterval}
-          setRepeatInterval={setRepeatInterval}
-          repeatEndDate={repeatEndDate}
-          setRepeatEndDate={setRepeatEndDate}
-        />
-      )}
+      {isRepeating && <RepeatTimeForm />}
 
       <Button data-testid="event-submit-button" onClick={addOrUpdateEvent} colorScheme="blue">
         {editingEvent ? '일정 수정' : '일정 추가'}
