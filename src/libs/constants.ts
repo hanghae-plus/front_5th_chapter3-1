37df4919bf2c52eaeaac4d7eb62d2e6a1321841d
KumJungMin <rmawjdals@naver.com>
2025-05-15 22:08:24 +0900

    feat: 상수 추가하기

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -8,3 +8,13 @@ export const repeatOptions = [
     { label: '매년', value: 'yearly' },
   ],
 ] as const;
+
+export const categories = ['업무', '개인', '가족', '기타'];
+
+export const notificationOptions = [
+  { value: 1, label: '1분 전' },
+  { value: 10, label: '10분 전' },
+  { value: 60, label: '1시간 전' },
+  { value: 120, label: '2시간 전' },
+  { value: 1440, label: '1일 전' },
+];
