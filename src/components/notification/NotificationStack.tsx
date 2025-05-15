7d914fb459c1fc8055c57ad2406e0eb53fc65ae5
KumJungMin <rmawjdals@naver.com>
2025-05-15 22:13:01 +0900

    feat: 공지 내역에 컨텍스트 적용하기

--- src/components/notification/NotificationStack.tsx
+++ src/components/notification/NotificationStack.tsx
@@ -1,21 +1,26 @@
 import { Alert, AlertIcon, AlertTitle, Box, CloseButton, VStack } from '@chakra-ui/react';
 
-const NotificationStack = (props) => {
-  const { notifications, setNotifications } = props;
+import { useNotificationsContext } from '../../contexts/NotificationsContext';
+
+const NotificationStack = () => {
+  const { notifications, setNotifications } = useNotificationsContext();
+
   return (
-    <VStack position="fixed" top={4} right={4} spacing={2} align="flex-end">
-      {notifications.map((notification, index) => (
-        <Alert key={index} status="info" variant="solid" width="auto">
-          <AlertIcon />
-          <Box flex="1">
-            <AlertTitle fontSize="sm">{notification.message}</AlertTitle>
-          </Box>
-          <CloseButton
-            onClick={() => setNotifications((prev) => prev.filter((_, i) => i !== index))}
-          />
-        </Alert>
-      ))}
-    </VStack>
+    notifications.length > 0 && (
+      <VStack position="fixed" top={4} right={4} spacing={2} align="flex-end">
+        {notifications.map((notification, index) => (
+          <Alert key={index} status="info" variant="solid" width="auto">
+            <AlertIcon />
+            <Box flex="1">
+              <AlertTitle fontSize="sm">{notification.message}</AlertTitle>
+            </Box>
+            <CloseButton
+              onClick={() => setNotifications((prev) => prev.filter((_, i) => i !== index))}
+            />
+          </Alert>
+        ))}
+      </VStack>
+    )
   );
 };
 
