0f3d5e22dcdf70d01a9856e7d152de6b6be02dc1
Paul Kang <kbu715@gmail.com>
2025-05-15 23:16:22 +0900

    refactor: OverlapAlertDialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
 import {
-  AlertDialog,
-  AlertDialogBody,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogOverlay,
   Box,
   Button,
   Checkbox,
@@ -15,7 +9,6 @@ import {
   HStack,
   Input,
   Select,
-  Text,
   Tooltip,
   useToast,
   VStack,
@@ -32,7 +25,7 @@ import {
 
 import { Event, EventForm, RepeatType } from '@/types';
 import { findOverlappingEvents, getTimeErrorMessage } from '@/utils';
-import { Calendar, EventList, NotificationList } from '@/components';
+import { Calendar, EventList, NotificationList, OverlapAlertDialog } from '@/components';
 import { categories, notificationOptions } from '@/lib';
 
 function App() {
@@ -275,60 +268,31 @@ function App() {
         />
       </Flex>
 
-      <AlertDialog
+      <OverlapAlertDialog
         isOpen={isOverlapDialogOpen}
-        leastDestructiveRef={cancelRef}
+        cancelRef={cancelRef}
         onClose={closeDialog}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize="lg" fontWeight="bold">
-              일정 겹침 경고
-            </AlertDialogHeader>
-
-            <AlertDialogBody>
-              다음 일정과 겹칩니다:
-              {overlappingEvents.map((event) => (
-                <Text key={event.id}>
-                  {event.title} ({event.date} {event.startTime}-{event.endTime})
-                </Text>
-              ))}
-              계속 진행하시겠습니까?
-            </AlertDialogBody>
-
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={closeDialog}>
-                취소
-              </Button>
-              <Button
-                colorScheme="red"
-                onClick={() => {
-                  closeDialog();
-                  saveEvent({
-                    id: editingEvent ? editingEvent.id : undefined,
-                    title,
-                    date,
-                    startTime,
-                    endTime,
-                    description,
-                    location,
-                    category,
-                    repeat: {
-                      type: isRepeating ? repeatType : 'none',
-                      interval: repeatInterval,
-                      endDate: repeatEndDate || undefined,
-                    },
-                    notificationTime,
-                  });
-                }}
-                ml={3}
-              >
-                계속 진행
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
+        overlappingEvents={overlappingEvents}
+        onContinue={() => {
+          closeDialog();
+          saveEvent({
+            id: editingEvent ? editingEvent.id : undefined,
+            title,
+            date,
+            startTime,
+            endTime,
+            description,
+            location,
+            category,
+            repeat: {
+              type: isRepeating ? repeatType : 'none',
+              interval: repeatInterval,
+              endDate: repeatEndDate || undefined,
+            },
+            notificationTime,
+          });
+        }}
+      />
 
       <NotificationList />
     </Box>
