297396e9c2171290a1624310ff22e737750035bb
HoJae Lee <hojae.lee@softly.ai>
2025-05-15 20:11:09 +0900

    refactor: useEventForm hook to use with context

--- src/__tests__/medium.integration.spec.tsx
+++ src/__tests__/medium.integration.spec.tsx
@@ -1,4 +1,3 @@
-import { ChakraProvider } from '@chakra-ui/react';
 import { render, screen, within, waitFor } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 
@@ -8,6 +7,7 @@ import {
   setupMockHandlerUpdating,
 } from '../__mocks__/handlersUtils';
 import App from '../App';
+import { AppProviders } from '../AppProviders';
 import { server } from '../setupTests';
 import { Event } from '../types';
 import { getTestEvents } from './fixtures/eventFactory';
@@ -28,7 +28,7 @@ describe('일정 관리 기능 테스트', () => {
     it('입력한 새로운 일정 정보에 맞춰 모든 필드가 이벤트 리스트에 정확히 저장된다.', async () => {
       // ! HINT. event를 추가 제거하고 저장하는 로직을 잘 살펴보고, 만약 그대로 구현한다면 어떤 문제가 있을 지 고민해보세요.
       server.use(...setupMockHandlerCreation(events as Event[]));
-      render(<App />, { wrapper: ChakraProvider });
+      render(<App />, { wrapper: AppProviders });
 
       await user.type(screen.getByLabelText('제목'), '팀 미팅');
       await user.type(screen.getByLabelText('날짜'), '2025-05-27');
@@ -62,7 +62,7 @@ describe('일정 관리 기능 테스트', () => {
 
     it('기존 일정의 세부 정보를 수정하고 변경사항이 정확히 반영된다', async () => {
       server.use(...setupMockHandlerUpdating(events as Event[]));
-      render(<App />, { wrapper: ChakraProvider });
+      render(<App />, { wrapper: AppProviders });
 
       const eventList = screen.getByTestId('event-list');
 
@@ -83,7 +83,7 @@ describe('일정 관리 기능 테스트', () => {
 
     it('일정을 삭제하고 더 이상 조회되지 않는지 확인한다', async () => {
       server.use(...setupMockHandlerDeletion(events as Event[]));
-      render(<App />, { wrapper: ChakraProvider });
+      render(<App />, { wrapper: AppProviders });
 
       const eventList = screen.getByTestId('event-list');
 
@@ -104,7 +104,7 @@ describe('일정 관리 기능 테스트', () => {
   describe('일정 뷰', () => {
     it('주별 뷰를 선택 후 해당 주에 일정이 없으면, 일정이 표시되지 않는다.', async () => {
       server.use(...setupMockHandlerCreation(events as Event[]));
-      render(<App />, { wrapper: ChakraProvider });
+      render(<App />, { wrapper: AppProviders });
 
       const viewSelect = screen.getByRole('combobox', { name: /view/i });
       await user.selectOptions(viewSelect, 'week');
@@ -130,7 +130,7 @@ describe('일정 관리 기능 테스트', () => {
 
     it('주별 뷰 선택 후 해당 일자에 일정이 존재한다면 해당 일정이 정확히 표시된다', async () => {
       server.use(...setupMockHandlerCreation(events as Event[]));
-      render(<App />, { wrapper: ChakraProvider });
+      render(<App />, { wrapper: AppProviders });
 
       const viewSelect = screen.getByRole('combobox', { name: /view/i });
       await user.selectOptions(viewSelect, 'week');
@@ -146,7 +146,7 @@ describe('일정 관리 기능 테스트', () => {
 
     it('월별 뷰에 일정이 없으면, 일정이 표시되지 않아야 한다.', async () => {
       server.use(...setupMockHandlerCreation(events as Event[]));
-      render(<App />, { wrapper: ChakraProvider });
+      render(<App />, { wrapper: AppProviders });
 
       await user.click(screen.getByLabelText('Next'));
 
@@ -163,7 +163,7 @@ describe('일정 관리 기능 테스트', () => {
 
     it('월별 뷰에 일정이 정확히 표시되는지 확인한다', async () => {
       server.use(...setupMockHandlerCreation(events as Event[]));
-      render(<App />, { wrapper: ChakraProvider });
+      render(<App />, { wrapper: AppProviders });
 
       const monthView = screen.getByTestId('month-view');
 
@@ -178,7 +178,7 @@ describe('일정 관리 기능 테스트', () => {
 
     it('달력에 1월 1일(신정)이 공휴일로 표시되는지 확인한다', async () => {
       server.use(...setupMockHandlerCreation([]));
-      render(<App />, { wrapper: ChakraProvider });
+      render(<App />, { wrapper: AppProviders });
 
       for (let i = 0; i < 4; i++) {
         await user.click(screen.getByLabelText('Previous'));
@@ -197,7 +197,7 @@ describe('일정 관리 기능 테스트', () => {
   describe('검색 기능', () => {
     it('검색 결과가 없으면, "검색 결과가 없습니다."가 표시되어야 한다.', async () => {
       server.use(...setupMockHandlerCreation(events as Event[]));
-      render(<App />, { wrapper: ChakraProvider });
+      render(<App />, { wrapper: AppProviders });
 
       const eventList = screen.getByTestId('event-list');
 
@@ -211,7 +211,7 @@ describe('일정 관리 기능 테스트', () => {
 
     it("'팀 회의'를 검색하면 해당 제목을 가진 일정이 리스트에 노출된다", async () => {
       server.use(...setupMockHandlerCreation(events as Event[]));
-      render(<App />, { wrapper: ChakraProvider });
+      render(<App />, { wrapper: AppProviders });
 
       const eventList = screen.getByTestId('event-list');
       const searchInput = within(eventList).getByPlaceholderText('검색어를 입력하세요');
@@ -223,7 +223,7 @@ describe('일정 관리 기능 테스트', () => {
 
     it('검색어를 지우면 모든 일정이 다시 표시되어야 한다', async () => {
       server.use(...setupMockHandlerCreation(events as Event[]));
-      render(<App />, { wrapper: ChakraProvider });
+      render(<App />, { wrapper: AppProviders });
 
       const eventList = screen.getByTestId('event-list');
       const searchInput = within(eventList).getByPlaceholderText('검색어를 입력하세요');
@@ -244,7 +244,7 @@ describe('일정 관리 기능 테스트', () => {
   describe('일정 충돌', () => {
     it('겹치는 시간에 새 일정을 추가할 때 경고가 표시된다', async () => {
       server.use(...setupMockHandlerCreation(events as Event[]));
-      render(<App />, { wrapper: ChakraProvider });
+      render(<App />, { wrapper: AppProviders });
 
       await user.type(screen.getByLabelText('제목'), '팀 미팅');
       await user.type(screen.getByLabelText('날짜'), '2025-05-25');
@@ -260,7 +260,7 @@ describe('일정 관리 기능 테스트', () => {
 
     it('기존 일정의 시간을 수정하여 충돌이 발생하면 경고가 노출된다', async () => {
       server.use(...setupMockHandlerUpdating(events as Event[]));
-      render(<App />, { wrapper: ChakraProvider });
+      render(<App />, { wrapper: AppProviders });
 
       const eventList = screen.getByTestId('event-list');
 
@@ -295,7 +295,7 @@ describe('알림 기능 테스트', () => {
   it('notificationTime을 1로 하면 지정 시간 1분 전 알람 텍스트가 노출된다', async () => {
     server.use(...setupMockHandlerCreation(events));
 
-    render(<App />, { wrapper: ChakraProvider });
+    render(<App />, { wrapper: AppProviders });
 
     const notification = await screen.findByText('1분 후 팀 회의 일정이 시작됩니다.');
     expect(notification).toBeInTheDocument();
