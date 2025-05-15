1e4430aad94dcc2ec500fb498a4de86aa32009e5
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-05-16 00:06:38 +0900

    Test: useEventOperations delete 수정

--- src/__tests__/hooks/medium.useEventOperations.spec.ts
+++ src/__tests__/hooks/medium.useEventOperations.spec.ts
@@ -86,11 +86,9 @@ it('존재하는 이벤트 삭제 시 에러없이 아이템이 삭제된다.', 
 
   await waitFor(() => result.current.events.length > 0);
 
-  const initEventCount = result.current.events.length;
-  console.log(result.current.events);
   await waitFor(async () => await result.current.deleteEvent('1'));
 
-  expect(result.current.events.length).toEqual(initEventCount - 1);
+  expect(result.current.events.length).toEqual(mockEvents.length - 1);
 
   expect(mockToast).toHaveBeenCalledWith({
     title: '일정이 삭제되었습니다.',
@@ -102,6 +100,7 @@ it('존재하는 이벤트 삭제 시 에러없이 아이템이 삭제된다.', 
 
 it("이벤트 로딩 실패 시 '이벤트 로딩 실패'라는 텍스트와 함께 에러 토스트가 표시되어야 한다", async () => {
   //fetchEvents에서 fetch를 하는경우 server.use로 에러를 반호나하여 로딩실패를 유도
+  server.use();
 });
 
 it("존재하지 않는 이벤트 수정 시 '일정 저장 실패'라는 토스트가 노출되며 에러 처리가 되어야 한다", async () => {
