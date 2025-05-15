8c2a4fda0a426c0198056c9e478633fe0c85030f
sj.kim <sj.kim@greendote.co.kr>
2025-05-15 17:13:11 +0900

    feat. notificationUtils 테스트 코드 구현

--- src/__mocks__/fixtures.ts
+++ src/__mocks__/fixtures.ts
@@ -92,3 +92,30 @@ export const filteredTestEvents: Event[] = [
     notificationTime: 1,
   },
 ];
+
+export const upcomingTestEvents: Event[] = [
+  {
+    id: '1',
+    title: '이벤트 1',
+    date: '2025-07-01',
+    startTime: '10:00',
+    endTime: '11:00',
+    description: '이벤트 1 설명',
+    location: '이벤트 1 장소',
+    category: '이벤트 1 카테고리',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 30,
+  },
+  {
+    id: '2',
+    title: '이벤트 2',
+    date: '2025-07-01',
+    startTime: '12:00',
+    endTime: '13:00',
+    description: '이벤트 2 설명',
+    location: '이벤트 2 장소',
+    category: '이벤트 2 카테고리',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 1,
+  },
+];
