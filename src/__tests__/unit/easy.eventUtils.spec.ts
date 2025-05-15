608946397fd609bcdce448856f23398a1029b06e
codeming <hmk2000015@gmail.com>
2025-05-16 04:25:00 +0900

    Update easy.eventUtils.spec.ts

--- src/__tests__/unit/easy.eventUtils.spec.ts
+++ src/__tests__/unit/easy.eventUtils.spec.ts
@@ -132,31 +132,34 @@ describe('getFilteredEvents', () => {
     const events: Event[] = [
       {
         id: '1',
+        description: '이벤트 1',
         title: '이벤트 1',
+        location: '장소 1',
         date: '2025-07-01',
         startTime: '14:30',
         endTime: '15:30',
-        description: '이벤트 1 설명',
-        location: '회의실 A',
         category: '업무',
         repeat: { type: 'none', interval: 0 },
         notificationTime: 0,
       },
       {
         id: '2',
+        description: '이벤트 2',
         title: '이벤트 2',
-        date: '2025-07-31',
+        location: '장소 2',
+        date: '2025-08-01',
         startTime: '14:30',
         endTime: '15:30',
-        description: '이벤트 2 설명',
-        location: '회의실 B',
         category: '업무',
         repeat: { type: 'none', interval: 0 },
         notificationTime: 0,
       },
     ];
-    const check = getFilteredEvents(events, '', new Date('2025-07-31'), 'month');
-    expect(check).toEqual([events[0]]);
+
+    const currentDate = new Date('2025-07-01');
+    const view = 'month';
+    const filteredEvents = getFilteredEvents(events, '', currentDate, view);
+    expect(filteredEvents).toEqual([events[0]]);
   });
   it('빈 이벤트 리스트에 대해 빈 배열을 반환한다', () => {
     const events: Event[] = [];
