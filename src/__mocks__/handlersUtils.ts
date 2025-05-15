1afde0c2dad8204d8275e95721afac8be24ea290
서주희 <maywngml@naver.com>
2025-05-15 21:50:58 +0900

    handlersUtils > setupMockHandlerUpdating에 event 매개변수 추가

--- src/__mocks__/handlersUtils.ts
+++ src/__mocks__/handlersUtils.ts
@@ -1,9 +1,9 @@
+import { server } from '../setupTests';
+import { events } from '../__mocks__/response/events.json' assert { type: 'json' };
+import { Event } from '../types';
 import { http, HttpResponse } from 'msw';
 
-import { server } from '../setupTests';
-import { Event } from '../types';
-
-export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {
+export const setupMockHandlerCreation = (initEvents = events as Event[]) => {
   const mockEvents: Event[] = [...initEvents];
 
   server.use(
@@ -19,33 +19,9 @@ export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {
   );
 };
 
-export const setupMockHandlerUpdating = () => {
-  const mockEvents: Event[] = [
-    {
-      id: '1',
-      title: '기존 회의',
-      date: '2025-10-15',
-      startTime: '09:00',
-      endTime: '10:00',
-      description: '기존 팀 미팅',
-      location: '회의실 B',
-      category: '업무',
-      repeat: { type: 'none', interval: 0 },
-      notificationTime: 10,
-    },
-    {
-      id: '2',
-      title: '기존 회의2',
-      date: '2025-10-15',
-      startTime: '11:00',
-      endTime: '12:00',
-      description: '기존 팀 미팅 2',
-      location: '회의실 C',
-      category: '업무 회의',
-      repeat: { type: 'none', interval: 0 },
-      notificationTime: 5,
-    },
-  ];
+export const setupMockHandlerUpdating = (initEvents = events as Event[]) => {
+    const mockEvents: Event[] = [...initEvents];
+
 
   server.use(
     http.get('/api/events', () => {
