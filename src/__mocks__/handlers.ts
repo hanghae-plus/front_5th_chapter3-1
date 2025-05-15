36d566bac131b981b514e5fa6931a92688d1dfcd
Doh Kim <ehgud456456@naver.com>
2025-05-15 15:57:53 +0900

    fix: handlers의 mock event도 const로 변경

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -5,7 +5,7 @@ import { http, HttpResponse } from 'msw';
 import { Event } from '../types';
 import { events } from './response/events.json' assert { type: 'json' };
 
-let mockEvents = [...events];
+const mockEvents = [...events];
 
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
@@ -20,16 +20,23 @@ export const handlers = [
     return HttpResponse.json({ success: true }, { status: 201 });
   }),
 
-  http.put('/api/events/:id', async ({ request, params }) => {
-    const { id } = params;
+  http.put('/api/events/:id', async ({ request }) => {
     const updatedEvent = (await request.json()) as Event;
-    mockEvents = mockEvents.map((event) => (event.id === id ? updatedEvent : event));
+    const eventIndex = mockEvents.findIndex((event) => event.id === updatedEvent.id);
+    if (eventIndex === -1) {
+      return HttpResponse.json({ success: false }, { status: 404 });
+    }
+    mockEvents[eventIndex] = updatedEvent;
     return HttpResponse.json({ success: true }, { status: 200 });
   }),
 
   http.delete('/api/events/:id', ({ params }) => {
     const { id } = params;
-    mockEvents = mockEvents.filter((event) => event.id !== id);
+    const eventIndex = mockEvents.findIndex((event) => event.id === id);
+    if (eventIndex === -1) {
+      return HttpResponse.json({ success: false }, { status: 404 });
+    }
+    mockEvents.splice(eventIndex, 1);
     return HttpResponse.json({ success: true }, { status: 200 });
   }),
 ];
