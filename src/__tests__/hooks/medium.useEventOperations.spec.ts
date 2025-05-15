8b5266ec5820ff85e9ca4c04c67f7aec4a3ce3ca
Doh Kim <ehgud456456@naver.com>
2025-05-15 16:06:22 +0900

    fix: console.error mocking

--- src/__tests__/hooks/medium.useEventOperations.spec.ts
+++ src/__tests__/hooks/medium.useEventOperations.spec.ts
@@ -209,6 +209,9 @@ it("이벤트 로딩 실패 시 '이벤트 로딩 실패'라는 텍스트와 함
 
 it("존재하지 않는 이벤트 수정 시 '일정 저장 실패'라는 토스트가 노출되며 에러 처리가 되어야 한다", async () => {
   setupMockHandlerUpdating(events as Event[]);
+  const originalConsoleError = console.error;
+  console.error = vi.fn();
+
   const { result } = renderHook(() => useEventOperations(true));
 
   const event = events[0] as Event;
@@ -233,6 +236,7 @@ it("존재하지 않는 이벤트 수정 시 '일정 저장 실패'라는 토스
   });
 
   expect(result.current.events).toEqual(events);
+  console.error = originalConsoleError;
 });
 
 it("네트워크 오류 시 '일정 삭제 실패'라는 텍스트가 노출되며 이벤트 삭제가 실패해야 한다", async () => {
